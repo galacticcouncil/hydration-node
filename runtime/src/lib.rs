8beec31ef9d351ea200121df2916a990b80bfea9
Martin Hloska <martin.hloska@topmonks.com>
2020-12-02 22:42:20 +0100

    clippy happy - removed unused import

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -26,8 +26,6 @@ use sp_std::prelude::*;
 use sp_version::NativeVersion;
 use sp_version::RuntimeVersion;
 
-use pallet_transaction_payment::CurrencyAdapter;
-
 // A few exports that help ease life for downstream crates.
 pub use frame_support::{
 	construct_runtime, parameter_types,
