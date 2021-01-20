7cabac94f25a44d47f3abecd76c677bff536d4c4
martinfridrich <mrtnfridrich@gmail.com>
2021-01-20 17:20:39 +0100

    fixed PR comments

--- runtime/src/constants.rs
+++ runtime/src/constants.rs
@@ -31,7 +31,7 @@ pub mod time {
 	///
 	/// <https://research.web3.foundation/en/latest/polkadot/BABE/Babe/#6-practical-results>
 
-	pub const MILLISECS_PER_BLOCK: Moment = 10000;
+	pub const MILLISECS_PER_BLOCK: Moment = 6_000;
 	pub const SECS_PER_BLOCK: Moment = MILLISECS_PER_BLOCK / 1000;
 
 	pub const MINUTES: BlockNumber = 60 / (SECS_PER_BLOCK as BlockNumber);
--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -134,15 +134,6 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	transaction_version: 1,
 };
 
-pub const MILLISECS_PER_BLOCK: u64 = 6000;
-
-pub const SLOT_DURATION: u64 = MILLISECS_PER_BLOCK;
-
-// Time is measured by number of blocks.
-pub const MINUTES: BlockNumber = 60_000 / (MILLISECS_PER_BLOCK as BlockNumber);
-pub const HOURS: BlockNumber = MINUTES * 60;
-pub const DAYS: BlockNumber = HOURS * 24;
-
 /// We assume that an on-initialize consumes 2.5% of the weight on average, hence a single extrinsic
 /// will not be allowed to consume more than `AvailableBlockRatio - 2.5%`.
 pub const AVERAGE_ON_INITIALIZE_RATIO: Perbill = Perbill::from_perthousand(25);
