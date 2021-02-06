cf65a1982a357b70db68ebcfa7bac3a9231263ff
Lumir Mrkva <lumir.mrkva@topmonks.com>
2021-02-06 01:24:44 +0000

    snakenet ftw

--- node/src/command.rs
+++ node/src/command.rs
@@ -23,7 +23,7 @@ use sc_service::PartialComponents;
 
 impl SubstrateCli for Cli {
 	fn impl_name() -> String {
-		"Substrate Node".into()
+		"hydra-dx snakenet".into()
 	}
 
 	fn impl_version() -> String {
