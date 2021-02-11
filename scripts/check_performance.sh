983b626194b43e2dd660b86775ac9315c4312d81
Martin Hloska <martin.hloska@gmail.com>
2021-02-11 12:42:57 +0100

    update perf check script to verify the toolchain

--- scripts/check_performance.sh
+++ scripts/check_performance.sh
@@ -1,7 +1,15 @@
-#!/bin/sh
+#!/bin/bash
 
 # Need to run from the top-level node directory
 [ -d ".maintain" ] || { echo "This script must be executed from the top level node directory"; exit 1; }
+
+echo "HydraDX node - Simple Performance check"
+echo "---------------------------------------"
+
+echo
+echo "Prerequisites"
+
+echo -n "Python version >= 3.8 ..... "
 
 # Need python3
 command -v python3 >/dev/null 2>&1 || { echo "python3 required. Please install first"; exit 1; }
@@ -10,6 +18,22 @@ if ! python3 -c 'import sys; assert sys.version_info >= (3,8)' > /dev/null 2>&1;
   echo "Python version 3.8 or higher required."
   exit 1
 fi
+
+echo "OK"
+
+echo -n "Toolchain ,,,,,,, "
+TOOLCHAIN=`rustup default`
+
+if [[ $TOOLCHAIN = "nightly"* ]]
+then
+        echo "OK"
+else
+        echo "Nightly toolchain required"
+        echo "Current toolchain $TOOLCHAIN"
+        exit 1
+fi
+
+echo
 
 # Run the check
 python3 .maintain/bench-check/bench_check.py $*
