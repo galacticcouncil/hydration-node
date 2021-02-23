1e09a9b1fb37fff05aaa52fd7593334cf5456850
Martin <martin.hloska@gmail.com>
2021-02-23 15:24:29 +0100

    Benchmarking performance check update (#101)
    * exchange weights updated
    
    * amm weights updated
    
    * transaction multi payment pallet weights update
    
    * perf check - increased margin to 15% and updated the expected weights
    
    * perf check tool minor changes
    
    * perf check tool better output
    
    * update perf check script to verify the toolchain
    
    * update the bench perf script
    
    * Update bench_check.py
    
    Go back to 10% 15% is too much when thinking about it.
    
    Co-authored-by: hydra-bench-bot <martin@hydradx.io>
    Co-authored-by: Jakub Pánik <jakub.panik@gmail.com>

--- .maintain/bench-check/bench_check.py
+++ .maintain/bench-check/bench_check.py
@@ -14,7 +14,7 @@ COMMAND = [
     '--',
     'benchmark',
     '--chain=dev',
-    '--steps=50',
+    '--steps=5',
     '--repeat=20',
     '--extrinsic=*',
     '--execution=wasm',
@@ -22,7 +22,7 @@ COMMAND = [
     '--heap-pages=4096',
 ]
 
-PALLETs = ["amm", "exchange", "transaction_multi_payment"]
+PALLETS = ["amm", "exchange", "transaction_multi_payment"]
 
 def load_hydra_values(filename):
     with open(filename,"r") as f:
@@ -37,7 +37,7 @@ def run_benchmarks():
     print("Running benchmarks - this may take a while...")
 
     results = defaultdict(dict)
-    for pallet in PALLETs:
+    for pallet in PALLETS:
         command = COMMAND + [f"--pallet={pallet}"]
 
         result = subprocess.run(command, capture_output=True)
@@ -47,15 +47,20 @@ def run_benchmarks():
         for idx, line in enumerate(lines):
             if line.startswith("Pallet:"):
                 info = line.split(",")
-                pallet = info[0].split(":")[1].strip()[1:-1]
+                pallet_name = info[0].split(":")[1].strip()[1:-1]
                 extrinsic = info[1].split(":")[1].strip()[1:-1]
-                results[pallet][extrinsic] = process_extrinsic(lines[idx+1:idx+21])
+                results[pallet_name][extrinsic] = process_extrinsic(lines[idx+1:idx+21])
 
     return results
 
 def show_pallet_result(pallet, hydra_data, current_data):
     hydra = sum(list(map(lambda x:float(x), hydra_data.values())))
-    current = sum(list(map(lambda x:float(x), current_data.values())))
+
+    c = []
+    for key in hydra_data.keys():
+        c.append(current_data[key])
+
+    current = sum(list(map(lambda x:float(x), c)))
 
     hydra_margin = int(hydra * DIFF_MARGIN / 100)
 
@@ -64,7 +69,7 @@ def show_pallet_result(pallet, hydra_data, current_data):
     note = "OK" if diff >= -hydra_margin else "FAILED"
 
     diff = f"{diff}"
-    times = f"{hydra} vs {current}"
+    times = f"{hydra:.2f} vs {current:.2f}"
 
     print(f"{pallet:<25}| {times:^25} | {diff:^13} | {note:^10}")
 
@@ -87,8 +92,8 @@ if __name__ == '__main__':
 
     print("\nNotes:")
     print("* - diff means the difference between HydraDX reference total time and total benchmark time of current machine")
-    print("* - If diff >= 0 - ( 10% of ref value) -> performance is same or better")
-    print("* - If diff < 0 - ( 10% of ref value) -> performance is worse and might not be suitable to run HydraDX node ( You may ask HydraDX devs for further clarifications)")
+    print(f"* - If diff >= 0 - ( {DIFF_MARGIN} of ref value) -> performance is same or better")
+    print(f"* - If diff < 0 - ( {DIFF_MARGIN} of ref value) -> performance is worse and might not be suitable to run HydraDX node ( You may ask HydraDX devs for further clarifications)")
 
     #write_hydra_results(results, "scripts/h.json")
 
