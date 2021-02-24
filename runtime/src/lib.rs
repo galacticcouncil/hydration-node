fb68e2aaaf70afccbb5da5f350cf75d8fed730dd
Martin Hloska <martin.hloska@gmail.com>
2021-02-24 16:37:09 +0100

    bump runtime - version 2

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -125,7 +125,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("hack-hydra-dx"),
 	impl_name: create_runtime_str!("hack-hydra-dx"),
 	authoring_version: 1,
-	spec_version: 1,
+	spec_version: 2,
 	impl_version: 1,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
