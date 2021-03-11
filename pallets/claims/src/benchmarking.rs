0aae1cbb7a59a3e3cc6d7d03a03845a3f0a124f3
green-jay <greenee@email.cz>
2021-03-11 16:51:21 +0100

    fix: :bug: update Alice's signature in claims bench test

--- pallets/claims/src/benchmarking.rs
+++ pallets/claims/src/benchmarking.rs
@@ -14,11 +14,11 @@ benchmarks! {
 		let alice_id = hex!["d43593c715fdd31c61141abd04a99fd6822c8558854ccde39a5684e7a56da27d"];
 		let caller = T::AccountId::decode(&mut &alice_id[..]).unwrap_or_default();
 		let eth_address = EthereumAddress(hex!["8202c0af5962b750123ce1a9b12e1c30a4973557"]);
-		let signature = hex!["ef9816023122208983c11e596446874df3d400d2f9e380a831206d0e91bfb96d54db352fbd62d3cfa8d8674cf63e6a32052ef3cab038e1e7398eac3d048ed5181c"];
+		let signature = hex!["bcae7d4f96f71cf974c173ae936a1a79083af7f76232efbf8a568b7f990eceed73c2465bba769de959b7f6ac5690162b61eb90949901464d0fa158a83022a0741c"];
 		Claims::<T>::insert(eth_address, T::CurrencyBalance::from(1_000_000_000_000_000_000_u128));
 	}: _(RawOrigin::Signed(caller.clone()), EcdsaSignature(signature))
 	verify {
-		assert_eq!(T::Currency::free_balance(&caller), T::CurrencyBalance::from(2_152_921_504_606_846_975_u128).into());
+		assert_eq!(T::Currency::free_balance(&caller), T::CurrencyBalance::from(2_000_000_000_000_000_000_u128).into());
 	}
 }
 
