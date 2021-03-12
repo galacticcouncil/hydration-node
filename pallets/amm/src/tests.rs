d2896448e79d3d4ad244aaf201597d9dbe2f5451
Martin Hloska <martin.hloska@gmail.com>
2021-03-12 12:33:42 +0100

    test(amm): use account ids in test for new substrate events

--- pallets/amm/src/tests.rs
+++ pallets/amm/src/tests.rs
@@ -638,7 +638,7 @@ fn discount_sell_fees_should_work() {
 
 		expect_events(vec![
 			RawEvent::CreatePool(user_1, asset_a, HDX, 10_000).into(),
-			frame_system::Event::NewAccount(2003000).into(),
+			frame_system::Event::NewAccount(pair_account).into(),
 			RawEvent::CreatePool(user_1, asset_a, asset_b, 60_000).into(),
 			RawEvent::Sell(user_1, asset_a, asset_b, 10_000, 14_993).into(),
 		]);
@@ -718,6 +718,11 @@ fn single_buy_with_discount_should_work() {
 			Price::from(2)
 		));
 
+		let hdx_pair_account = AMM::get_pair_id(AssetPair {
+			asset_in: asset_a,
+			asset_out: HDX,
+		});
+
 		let pair_account = AMM::get_pair_id(AssetPair {
 			asset_in: asset_a,
 			asset_out: asset_b,
@@ -754,7 +759,7 @@ fn single_buy_with_discount_should_work() {
 
 		expect_events(vec![
 			RawEvent::CreatePool(user_1, asset_a, asset_b, 640_000_000_000).into(),
-			frame_system::Event::NewAccount(1003000).into(),
+			frame_system::Event::NewAccount(hdx_pair_account).into(),
 			RawEvent::CreatePool(user_1, asset_a, HDX, 100_000_000_000).into(),
 			RawEvent::Buy(user_1, asset_a, asset_b, 66_666_666, 320_336_108_035).into(),
 		]);
@@ -961,10 +966,10 @@ fn destroy_pool_on_remove_liquidity_and_recreate_should_work() {
 
 		expect_events(vec![
 			RawEvent::CreatePool(user, asset_a, asset_b, 100_000_000).into(),
-			frame_system::Event::KilledAccount(1002000).into(),
+			frame_system::Event::KilledAccount(pair_account).into(),
 			RawEvent::RemoveLiquidity(user, asset_a, asset_b, 100_000_000).into(),
 			RawEvent::PoolDestroyed(user, asset_a, asset_b).into(),
-			frame_system::Event::NewAccount(1002000).into(),
+			frame_system::Event::NewAccount(pair_account).into(),
 			RawEvent::CreatePool(user, asset_a, asset_b, 100_000_000).into(),
 		]);
 	});
