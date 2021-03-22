59ece5f951887d53903a3e487c31997b07d7b4f4
Martin Hloska <martin.hloska@gmail.com>
2021-03-22 11:03:25 +0100

    feat(perf-check): install bench wizard as part of the check perf script

--- scripts/check_performance.sh
+++ scripts/check_performance.sh
@@ -46,9 +46,22 @@ echo -n "benchwizard >= $EXPECTED_BENCHWIZARD_VERSION ..... "
 $PYTHON -m bench_wizard >/dev/null 2>&1 || {
   echo "benchwizard required. benchwizard is cli tool developed by HydraDX dev to streamline substrate benchmark process.";
   echo "Installation: pip3 install bench-wizard";
-  exit 1;
-  }
+  echo
+  read -p "Do you want to install it now? [Y/n] " -n 1 -r
+  echo    # move to a new line
+  if [[ ! $REPLY =~ ^[Yy]$ ]]
+  then
+      exit 1
+  fi
 
+  pip3 install bench-wizard > /dev/null
+
+  if [[ $? != 0 ]];
+  then
+    echo "benchwizard installation failed."
+    exit 1
+  fi
+}
 CURRENT_BENCH_VERSION=`$PYTHON -m bench_wizard version | tr -d '\n'`
 
 if [[ $EXPECTED_BENCHWIZARD_VERSION > $CURRENT_BENCH_VERSION ]]
