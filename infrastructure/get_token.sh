8a5da9c353bf60397603c952aa3f533bdac9a967
Ayoub <ayoub@fakir.dev>
2021-04-17 01:56:19 +0200

    test

--- infrastructure/get_token.sh
+++ infrastructure/get_token.sh
@@ -2,6 +2,7 @@
 header1="Accept: application/vnd.github.v3+json"
 header2="Authorization: token $1"
 apiUrl="https://api.github.com/orgs/galacticcouncil/actions/runners/registration-token"
+#apiUrl="https://api.github.com/repos/jak-pan/HydraDX-node/actions/runners/registration-token"
 
 cmd="$(curl -X POST -H "$header1" -H "$header2" "$apiUrl" | jq -r '.token')"
 echo "${cmd}"
