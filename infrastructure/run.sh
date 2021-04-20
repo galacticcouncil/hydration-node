97887024c40b83e16d7e023134691c1720ac5390
Ayoub <ayoub@fakir.dev>
2021-04-21 00:20:52 +0100

    run

--- infrastructure/run.sh
+++ infrastructure/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-bash_source=$(bash source /home/ubuntu/config_script.sh)
+bash_source=$(source /home/ubuntu/config_script.sh)
 echo $bash_source
 run=$(bash /home/ubuntu/config_script.sh $ACCESS_TOKEN &)
 echo $run
