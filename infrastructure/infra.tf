9506534d7eb13231e00f28b037cd571d9af5eaf0
Ayoub <ayoub@fakir.dev>
2021-04-21 03:13:19 +0100

    dynamic branch name

--- infrastructure/infra.tf
+++ infrastructure/infra.tf
@@ -50,7 +50,7 @@ resource "aws_instance" "runner-aws" {
     }
     provisioner "remote-exec" {
         inline = [
-        "tmux new -d 'bash run_conf.sh ${branch_name}'"
+        "tmux new -d 'bash run_conf.sh ${var.branch_name}'"
         ]
     }
 }
