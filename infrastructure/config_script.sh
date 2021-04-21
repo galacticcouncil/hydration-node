08d05ad5fb63db5255c596adb293a4eb9086c61b
Ayoub <ayoub@fakir.dev>
2021-04-21 01:26:41 +0100

    runs on aws

--- infrastructure/config_script.sh
+++ infrastructure/config_script.sh
@@ -4,5 +4,4 @@ tar xzf ./actions-runner-linux-x64-2.277.1.tar.gz
 TOKEN=$(bash get_token.sh $1)
 echo $TOKEN
 bash config.sh --url https://github.com/galacticcouncil/HydraDX-node --token $TOKEN --name "aws-runner" --work _work --labels aws
-
 bash ./run.sh
