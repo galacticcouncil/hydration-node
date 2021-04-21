3d50dddc4984b71f403576911929a586b50adc88
Ayoub <ayoub@fakir.dev>
2021-04-21 03:15:51 +0100

    dynamic branch name

--- infrastructure/run_conf.sh
+++ infrastructure/run_conf.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 bash_source=$(source /home/ubuntu/config_script.sh)
 echo $bash_source
-bashrun=$(bash -c "config_script.sh $ACCESS_TOKEN $1 &")
+bashrun=$(bash -c "config_script.sh $1 &")
 echo $bashrun
