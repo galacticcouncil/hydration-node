8005631273c7c983090f2b1c98fbb50143d8313b
Jakub Panik <jakub.panik@gmail.com>
2021-04-23 18:23:37 +0200

    bump version

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -132,7 +132,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("hydra-dx"),
 	impl_name: create_runtime_str!("hydra-dx"),
 	authoring_version: 1,
-	spec_version: 8,
+	spec_version: 9,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
