badc9d2a39e3be29b45db3873896654dd298b026
Martin Hloska <martin.hloska@gmail.com>
2021-05-11 14:50:39 +0200

    refactor(build-utils): add parameter for runtime name

--- node/build.rs
+++ node/build.rs
@@ -1,6 +1,6 @@
 use hydra_dx_build_script_utils::{generate_cargo_keys, rerun_if_git_head_changed};
 
 fn main() {
-	generate_cargo_keys().expect("Failed to generate version metadata");
+	generate_cargo_keys("hydra-dx-runtime").expect("Failed to generate version metadata");
 	rerun_if_git_head_changed();
 }
