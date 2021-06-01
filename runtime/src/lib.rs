107c38f4b1ce4bba9921152fae7db013e494a3e0
Roznovjak <richardroznovjak@gmail.com>
2021-06-02 01:15:57 +0200

    bump versions

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -149,7 +149,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	impl_name: create_runtime_str!("hydra-dx"),
 	authoring_version: 1,
 	spec_version: 15,
-	impl_version: 1,
+	impl_version: 2,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
 };
