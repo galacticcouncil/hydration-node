1b27e713cef09b9c5d7c914ac74f8e65dfe5982c
Roznovjak <richardroznovjak@gmail.com>
2021-06-01 12:58:30 +0200

    update version numbers

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -148,8 +148,8 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("hydra-dx"),
 	impl_name: create_runtime_str!("hydra-dx"),
 	authoring_version: 1,
-	spec_version: 16,
-	impl_version: 0,
+	spec_version: 15,
+	impl_version: 1,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
 };
