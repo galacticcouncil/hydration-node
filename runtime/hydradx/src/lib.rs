25c91bacf1169ee5126feaf70835467246b68811
Martin <martin.hloska@gmail.com>
2022-04-04 16:49:36 +0200

    happy clippy happy life

--- runtime/hydradx/src/lib.rs
+++ runtime/hydradx/src/lib.rs
@@ -33,7 +33,7 @@ use sp_core::{
 };
 use sp_runtime::{
 	create_runtime_str, generic, impl_opaque_keys,
-	traits::{AccountIdConversion, BlakeTwo256, Block as BlockT, IdentityLookup, Zero},
+	traits::{AccountIdConversion, BlakeTwo256, Block as BlockT, IdentityLookup},
 	transaction_validity::{TransactionSource, TransactionValidity},
 	ApplyExtrinsicResult, Perbill,
 };
