6edc538b92bf2c05bcfd98b7107270d7b9b18a66
Alexander Popiak <alexander.popiak@parity.io>
2022-06-28 16:08:08 +0200

    bump runtime crate and spec_version

--- runtime/testing-hydradx/src/lib.rs
+++ runtime/testing-hydradx/src/lib.rs
@@ -104,7 +104,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("testing-hydradx"),
 	impl_name: create_runtime_str!("testing-hydradx"),
 	authoring_version: 1,
-	spec_version: 106,
+	spec_version: 107,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
