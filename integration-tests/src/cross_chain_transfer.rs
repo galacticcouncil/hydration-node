2334fee7ee8f9ede9d619dd60ba72d9b8afcffc9
dmoka <mokabme@gmail.com>
2022-07-19 16:44:39 +0200

    add todo for ignored test

--- integration-tests/src/cross_chain_transfer.rs
+++ integration-tests/src/cross_chain_transfer.rs
@@ -63,6 +63,7 @@ fn polkadot_should_receive_asset_when_sent_from_hydra() {
 
 #[test]
 #[ignore]
+//TODO: it seems that the balance does not change on hydra. To be investigated
 fn hydra_should_receive_asset_when_transferred_from_basilisk() {
 	//Arrange
 	TestNet::reset();
