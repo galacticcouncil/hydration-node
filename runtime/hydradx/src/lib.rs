f8173de909b6877e530ebad9c67c8e7b5c804c8a
Martin <martin.hloska@gmail.com>
2022-11-08 17:58:56 +0100

    bump versions

--- runtime/hydradx/src/lib.rs
+++ runtime/hydradx/src/lib.rs
@@ -97,7 +97,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("hydradx"),
 	impl_name: create_runtime_str!("hydradx"),
 	authoring_version: 1,
-	spec_version: 111,
+	spec_version: 112,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
