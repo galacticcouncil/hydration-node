327d6b962b1f90fc1631dfc3d7b62d9f8698c11d
dmoka <mokabme@gmail.com>
2022-11-17 17:39:01 +0100

    add test for selling omnioool asset for stableswap

--- pallets/omnipool-subpools/src/tests/sell.rs
+++ pallets/omnipool-subpools/src/tests/sell.rs
@@ -14,6 +14,7 @@ use pretty_assertions::assert_eq;
 use sp_runtime::BoundedVec;
 
 const ALICE_INITIAL_ASSET_3_BALANCE: Balance = 1000 * ONE;
+const ALICE_INITIAL_ASSET_5_BALANCE: Balance = 5000 * ONE;
 const OMNIPOOL_INITIAL_ASSET_3_BALANCE: Balance = 3000 * ONE;
 const OMNIPOOL_INITIAL_ASSET_4_BALANCE: Balance = 4000 * ONE;
 const OMNIPOOL_INITIAL_ASSET_5_BALANCE: Balance = 5000 * ONE;
@@ -176,7 +177,7 @@ fn sell_should_work_when_both_asset_in_omnipool() {
 }
 
 #[test]
-fn sell_should_work_when_one_asset_in_omnipool_and_other_in_subpool() {
+fn sell_should_work_when_selling_stable_asset_for_omnipool_asset() {
 	ExtBuilder::default()
 		.with_registered_asset(ASSET_3)
 		.with_registered_asset(ASSET_4)
@@ -226,6 +227,61 @@ fn sell_should_work_when_one_asset_in_omnipool_and_other_in_subpool() {
 				omnipool_account,
 				ASSET_5,
 				OMNIPOOL_INITIAL_ASSET_5_BALANCE - amount_to_get
+			);
+		});
+}
+
+#[test]
+fn sell_should_work_when_selling_omnipool_asset_for_stableswap_asset() {
+	ExtBuilder::default()
+		.with_registered_asset(ASSET_3)
+		.with_registered_asset(ASSET_4)
+		.with_registered_asset(ASSET_5)
+		.with_registered_asset(SHARE_ASSET_AS_POOL_ID)
+		.add_endowed_accounts((LP1, 1_000, 5000 * ONE))
+		.add_endowed_accounts((Omnipool::protocol_account(), ASSET_3, OMNIPOOL_INITIAL_ASSET_3_BALANCE))
+		.add_endowed_accounts((Omnipool::protocol_account(), ASSET_4, OMNIPOOL_INITIAL_ASSET_4_BALANCE))
+		.add_endowed_accounts((Omnipool::protocol_account(), ASSET_5, OMNIPOOL_INITIAL_ASSET_5_BALANCE))
+		.add_endowed_accounts((ALICE, ASSET_5, ALICE_INITIAL_ASSET_5_BALANCE))
+		.with_initial_pool(FixedU128::from_float(0.5), FixedU128::from(1))
+		.build()
+		.execute_with(|| {
+			add_omnipool_token!(ASSET_3);
+			add_omnipool_token!(ASSET_4);
+			add_omnipool_token!(ASSET_5);
+
+			create_subpool!(SHARE_ASSET_AS_POOL_ID, ASSET_3, ASSET_4);
+
+			//Act
+			let amount_to_sell = 100 * ONE;
+			assert_ok!(OmnipoolSubpools::sell(
+				Origin::signed(ALICE),
+				ASSET_5,
+				ASSET_3,
+				amount_to_sell,
+				0
+			));
+
+			//Assert
+			let pool_account = AccountIdConstructor::from_assets(&vec![ASSET_3, ASSET_4], None);
+			let omnipool_account = Omnipool::protocol_account();
+
+			let amount_to_get = 2903401662274523;
+
+			assert_balance!(ALICE, ASSET_3, amount_to_get);
+			assert_balance!(ALICE, ASSET_4, 0);
+			assert_balance!(ALICE, ASSET_5, ALICE_INITIAL_ASSET_5_BALANCE - amount_to_sell);
+
+			assert_balance!(pool_account, ASSET_3, OMNIPOOL_INITIAL_ASSET_3_BALANCE - amount_to_get);
+			assert_balance!(pool_account, ASSET_4, OMNIPOOL_INITIAL_ASSET_4_BALANCE);
+			assert_balance!(pool_account, ASSET_5, 0);
+
+			assert_balance!(omnipool_account, ASSET_3, 0);
+			assert_balance!(omnipool_account, ASSET_4, 0);
+			assert_balance!(
+				omnipool_account,
+				ASSET_5,
+				OMNIPOOL_INITIAL_ASSET_5_BALANCE + amount_to_sell
 			);
 		});
 }
