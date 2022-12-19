ab44664e06ec997713428ff10161f24a1c9c94c0
Roznovjak <richardroznovjak@gmail.com>
2022-12-19 11:06:47 +0100

    formatting

--- integration-tests/src/lib.rs
+++ integration-tests/src/lib.rs
@@ -1,7 +1,7 @@
+mod call_filter;
+mod circuit_breaker;
 mod cross_chain_transfer;
 mod non_native_fee;
 mod omnipool_init;
 mod polkadot_test_net;
 mod vesting;
-mod call_filter;
-mod circuit_breaker;
