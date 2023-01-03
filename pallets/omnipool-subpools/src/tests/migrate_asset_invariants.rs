c02970cda6b8f85d789b65fdecb4fe849f92b924
dmoka <mokabme@gmail.com>
2023-01-03 09:53:34 +0100

    refactor migrate assets invariants

--- pallets/omnipool-subpools/src/tests/migrate_asset_invariants.rs
+++ pallets/omnipool-subpools/src/tests/migrate_asset_invariants.rs
@@ -2,8 +2,10 @@ use super::*;
 use crate::types::Balance;
 use crate::*;
 use proptest::prelude::*;
+use sp_runtime::traits::CheckedAdd;
+use sp_runtime::traits::CheckedDiv;
+use sp_runtime::traits::CheckedMul;
 use test_utils::assert_balance;
-
 proptest! {
 	//Spec: https://www.notion.so/Add-Omnipool-asset-to-existing-stableswap-subpool-permissioned-d7ece293a23546a186a385a51f53212c
 	#![proptest_config(ProptestConfig::with_cases(100))]
@@ -28,12 +30,22 @@ proptest! {
 			.add_endowed_accounts((Omnipool::protocol_account(), asset_3.asset_id, asset_3.amount))
 			.add_endowed_accounts((Omnipool::protocol_account(), asset_4.asset_id, asset_4.amount))
 			.add_endowed_accounts((Omnipool::protocol_account(), asset_5.asset_id, asset_5.amount))
+			.add_endowed_accounts((ALICE, asset_5.asset_id, 100 * ONE))
 			.with_initial_pool(FixedU128::from_float(0.5), FixedU128::from(1))
 			.build()
 			.execute_with(|| {
 				assert_ok!(Omnipool::add_token(Origin::root(), asset_3.asset_id, asset_3.price,Permill::from_percent(100),LP1));
 				assert_ok!(Omnipool::add_token(Origin::root(), asset_4.asset_id, asset_4.price,Permill::from_percent(100),LP1));
 				assert_ok!(Omnipool::add_token(Origin::root(), asset_5.asset_id, asset_5.price,Permill::from_percent(100),LP1));
+
+				//We need to add then sacrifice liquidity for asset5 to have protocol shares
+				let position_id: u32 = Omnipool::next_position_id();
+				assert_ok!(OmnipoolSubpools::add_liquidity(
+					Origin::signed(ALICE),
+					asset_5.asset_id,
+					100 * ONE
+				));
+				assert_ok!(Omnipool::sacrifice_position(Origin::signed(ALICE), position_id));
 
 				let asset_state_3 = Omnipool::load_asset_state(asset_3.asset_id).unwrap();
 				let asset_state_4 = Omnipool::load_asset_state(asset_4.asset_id).unwrap();
@@ -62,6 +74,10 @@ proptest! {
 
 				let stableswap_pool_share_asset_before_migration = Omnipool::load_asset_state(SHARE_ASSET_AS_POOL_ID).unwrap();
 
+				let q_s = FixedU128::from(stableswap_pool_share_asset_before_migration.hub_reserve);
+				let b_s = FixedU128::from(stableswap_pool_share_asset_before_migration.protocol_shares);
+				let s_s = FixedU128::from(stableswap_pool_share_asset_before_migration.shares);
+
 				//Act
 				assert_ok!(OmnipoolSubpools::migrate_asset_to_subpool(
 					Origin::root(),
@@ -69,14 +85,30 @@ proptest! {
 					asset_5.asset_id,
 				));
 
+				let omnipool_lrna_balance_after = get_lrna_of_omnipool_protocol_account();
+				let sum_q_k = omnipool_lrna_balance_before;
+				let sum_q_k_plus = omnipool_lrna_balance_after;
+
+				let stableswap_pool_share_asset = Omnipool::load_asset_state(SHARE_ASSET_AS_POOL_ID).unwrap();
+				let s_s_plus = stableswap_pool_share_asset.shares;
+				let u_s_plus = stableswap_pool_share_asset.reserve;
+
+				let q_s_plus = FixedU128::from(stableswap_pool_share_asset.hub_reserve);
+				let b_s_plus = FixedU128::from(stableswap_pool_share_asset.protocol_shares);
+
+				 let q_5 = FixedU128::from(asset_state_5.hub_reserve);
+				let b_5 = FixedU128::from(asset_state_5.protocol_shares);
+				let s_5 = FixedU128::from(asset_state_5.shares);
+
 				//Assert
 				let pool_account = AccountIdConstructor::from_assets(&vec![asset_3.asset_id, asset_4.asset_id, asset_5.asset_id], None);
 
-				//Check that the full amount of lrna has not been changed
-				let omnipool_lrna_balance_after = get_lrna_of_omnipool_protocol_account();
-				assert_eq!(omnipool_lrna_balance_before, omnipool_lrna_balance_after);
+				//Sum(Qk) = Sum(Qk+)
+				let left = sum_q_k;
+				let right = sum_q_k_plus;
+				assert_invariant_eq!(left, right);
 
-				//Check that we transfer the right reserve from omnipool to subpool
+				//No risk assets are accounted for: Ri = Rsi
 				assert_balance!(Omnipool::protocol_account(), asset_3.asset_id, 0);
 				assert_balance!(Omnipool::protocol_account(), asset_4.asset_id, 0);
 				assert_balance!(Omnipool::protocol_account(), asset_5.asset_id, 0);
@@ -84,16 +116,27 @@ proptest! {
 				assert_balance!(pool_account, asset_4.asset_id, asset_4_reserve);
 				assert_balance!(pool_account, asset_5.asset_id, asset_5_reserve);
 
-				//Spec: https://www.notion.so/Add-Omnipool-asset-to-existing-stableswap-subpool-permissioned-d7ece293a23546a186a385a51f53212c#eea9ff0460b343e2be1fad13388f247f
-				let stableswap_pool_share_asset = Omnipool::load_asset_state(SHARE_ASSET_AS_POOL_ID).unwrap();
-				assert_eq!(stableswap_pool_share_asset.shares, stableswap_pool_share_asset.reserve);
-				assert_eq!(stableswap_pool_share_asset.shares, asset_3_lrna + asset_4_lrna +  asset_5_lrna);
+				// Us+ = Ss+
+				let left = u_s_plus;
+				let right = s_s_plus;
+				assert_invariant_eq!(left, right);
 
-				//Spec: https://www.notion.so/Add-Omnipool-asset-to-existing-stableswap-subpool-permissioned-d7ece293a23546a186a385a51f53212c
-				let left_expression = stableswap_pool_share_asset.hub_reserve * stableswap_pool_share_asset.protocol_shares / stableswap_pool_share_asset.shares;
-				let right_expression_for_asset5 = asset_state_5.hub_reserve * asset_state_5.protocol_shares / asset_state_5.shares;
-				let right_expression_for_share_asset_before_migration = stableswap_pool_share_asset_before_migration.hub_reserve * stableswap_pool_share_asset_before_migration.protocol_shares / stableswap_pool_share_asset_before_migration.shares;
-				assert_eq!(left_expression, right_expression_for_asset5 + right_expression_for_share_asset_before_migration);
+				// Ss+ = sum_Qk
+				let sum_q_k = asset_3_lrna + asset_4_lrna + asset_5_lrna;
+				let left = s_s_plus;
+				let right = sum_q_k;
+				 assert_invariant_eq!(left, right);
+
+				// Qs+ * Bs+/Ss+ = (Qi * Bi/Si) + (Qs * Bs/Ss)
+				let s_s_plus = FixedU128::from(s_s_plus);
+				let left = q_s_plus.checked_mul(&b_s_plus.checked_div(&s_s_plus).unwrap()).unwrap();
+				let right1 = q_5.checked_mul(&b_5.checked_div(&s_5).unwrap()).unwrap();
+				let right2 = q_s.checked_mul(&b_s.checked_div(&s_s).unwrap()).unwrap();
+				let right = right1.checked_add(&right2).unwrap();
+
+				#[cfg(feature = "all-invariants")]
+				assert_invariant_eq!(left, right);
+
 			});
 	}
 }
