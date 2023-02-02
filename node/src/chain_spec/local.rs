ca4fd1570433e8af2b530f9151a2375f8330f31c
Roznovjak <richardroznovjak@gmail.com>
2023-02-02 15:51:51 +0100

    use latest warehouse

--- node/src/chain_spec/local.rs
+++ node/src/chain_spec/local.rs
@@ -81,6 +81,7 @@ pub fn parachain_config() -> Result<ChainSpec, String> {
 				vec![],
 				// registered assets
 				vec![(b"KSM".to_vec(), 1_000u128), (b"KUSD".to_vec(), 1_000u128)],
+				vec![(b"KSM".to_vec(), 1_000u128, 1), (b"KUSD".to_vec(), 1_000u128, 2)],
 				// accepted assets
 				vec![(1, Price::from_float(0.0000212)), (2, Price::from_float(0.000806))],
 				// token balances
