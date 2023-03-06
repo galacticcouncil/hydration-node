e64b196ade6a33a8792914fde4a605593359491a
Roznovjak <richardroznovjak@gmail.com>
2023-03-06 20:17:10 +0100

    fix try-runtime cmd

--- node/src/command.rs
+++ node/src/command.rs
@@ -330,7 +330,9 @@ pub fn run() -> sc_cli::Result<()> {
 		}
 		#[cfg(feature = "try-runtime")]
 		Some(Subcommand::TryRuntime(cmd)) => {
+			use sc_executor::{sp_wasm_interface::ExtendedHostFunctions, NativeExecutionDispatch};
 			let runner = cli.create_runner(cmd)?;
+
 			runner.async_run(|config| {
 				// we don't need any of the components of new_partial, just a runtime, or a task
 				// manager to do `async_run`.
@@ -338,7 +340,7 @@ pub fn run() -> sc_cli::Result<()> {
 				let task_manager = sc_service::TaskManager::new(config.tokio_handle.clone(), registry)
 					.map_err(|e| sc_cli::Error::Service(sc_service::Error::Prometheus(e)))?;
 
-				Ok((cmd.run::<Block, service::HydraDXExecutorDispatch>(config), task_manager))
+				Ok((cmd.run::<Block, ExtendedHostFunctions<sp_io::SubstrateHostFunctions, <service::HydraDXExecutorDispatch as NativeExecutionDispatch>::ExtendHostFunctions>>(), task_manager))
 			})
 		}
 		#[cfg(not(feature = "try-runtime"))]
