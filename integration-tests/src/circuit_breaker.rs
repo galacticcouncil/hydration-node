8081d589488b843b3de29644c074e6ebb2bf54ec
dmoka <mokabme@gmail.com>
2023-03-17 17:30:33 +0100

    add buy integration tests

--- integration-tests/src/circuit_breaker.rs
+++ integration-tests/src/circuit_breaker.rs
@@ -6,6 +6,7 @@ use frame_system::RawOrigin;
 use hydradx_runtime::{Balances, CircuitBreaker, Omnipool, Tokens};
 use orml_traits::MultiCurrency;
 use primitives::constants::chain::CORE_ASSET_ID;
+use primitives::Balance;
 use sp_runtime::FixedU128;
 use sp_runtime::Permill;
 use xcm_emulator::TestExt;
@@ -20,18 +21,7 @@ use xcm_emulator::TestExt;
 fn sell_in_omnipool_should_work_when_max_trade_limit_per_block_not_exceeded() {
 	Hydra::execute_with(|| {
 		//Arrange
-		assert_ok!(hydradx_runtime::Omnipool::set_tvl_cap(
-			hydradx_runtime::Origin::root(),
-			222_222_000_000_000_000_000_000,
-		));
-
-		assert_ok!(Omnipool::initialize_pool(
-			RawOrigin::Root.into(),
-			FixedU128::from_float(0.00001), // adjust the amount of LRNA to roughly match the amount of LRNA that belongs to HDX. This way we can avoid MaxOutRatioExceeded error.
-			FixedU128::from(1),
-			Permill::from_percent(100),
-			Permill::from_percent(100)
-		));
+		init_omnipool();
 
 		let dai_balance_in_omnipool = Tokens::free_balance(DAI, &Omnipool::protocol_account());
 		let trade_volume_limit = CircuitBreaker::trade_volume_limit_per_asset(DAI);
@@ -62,18 +52,7 @@ fn sell_in_omnipool_should_work_when_max_trade_limit_per_block_not_exceeded() {
 fn sell_in_omnipool_should_fail_when_max_trade_limit_per_block_exceeded() {
 	Hydra::execute_with(|| {
 		//Arrange
-		assert_ok!(hydradx_runtime::Omnipool::set_tvl_cap(
-			hydradx_runtime::Origin::root(),
-			222_222_000_000_000_000_000_000,
-		));
-
-		assert_ok!(Omnipool::initialize_pool(
-			RawOrigin::Root.into(),
-			FixedU128::from_float(0.00001), // adjust the amount of LRNA to roughly match the amount of LRNA that belongs to HDX. This way we can avoid MaxOutRatioExceeded error.
-			FixedU128::from(1),
-			Permill::from_percent(100),
-			Permill::from_percent(100)
-		));
+		init_omnipool();
 
 		let dai_balance_in_omnipool = Tokens::free_balance(DAI, &Omnipool::protocol_account());
 		let trade_volume_limit = CircuitBreaker::trade_volume_limit_per_asset(DAI);
@@ -107,21 +86,62 @@ fn sell_in_omnipool_should_fail_when_max_trade_limit_per_block_exceeded() {
 }
 
 #[test]
+fn buy_in_omnipool_should_work_when_max_trade_limit_per_block_not_exceeded() {
+	Hydra::execute_with(|| {
+		//Arrange
+		init_omnipool();
+
+		assert_ok!(Tokens::set_balance(
+			RawOrigin::Root.into(),
+			ALICE.into(),
+			DAI,
+			10000000 * UNITS,
+			0,
+		));
+
+		assert_ok!(Omnipool::buy(
+			hydradx_runtime::Origin::signed(ALICE.into()),
+			CORE_ASSET_ID,
+			DAI,
+			1 * UNITS,
+			Balance::MAX
+		));
+	});
+}
+
+#[test]
+fn buy_in_omnipool_should_fail_when_max_trade_limit_per_block_exceeded() {
+	Hydra::execute_with(|| {
+		//Arrange
+		init_omnipool();
+
+		assert_ok!(Tokens::set_balance(
+			RawOrigin::Root.into(),
+			ALICE.into(),
+			DAI,
+			100000000000 * UNITS,
+			0,
+		));
+
+		//Act and assert
+		assert_noop!(
+			Omnipool::buy(
+				hydradx_runtime::Origin::signed(ALICE.into()),
+				CORE_ASSET_ID,
+				DAI,
+				100000 * UNITS,
+				Balance::MAX
+			),
+			pallet_circuit_breaker::Error::<hydradx_runtime::Runtime>::MaxTradeVolumePerBlockReached
+		);
+	});
+}
+
+#[test]
 fn add_liquidity_to_omnipool_should_work_when_liquidity_limit_per_block_not_exceeded() {
 	Hydra::execute_with(|| {
 		//Arrange
-		assert_ok!(hydradx_runtime::Omnipool::set_tvl_cap(
-			hydradx_runtime::Origin::root(),
-			222_222_000_000_000_000_000_000,
-		));
-
-		assert_ok!(Omnipool::initialize_pool(
-			RawOrigin::Root.into(),
-			FixedU128::from_float(0.65),
-			FixedU128::from(1),
-			Permill::from_percent(100),
-			Permill::from_percent(100)
-		));
+		init_omnipool();
 
 		let hdx_balance_in_omnipool = Balances::free_balance(&Omnipool::protocol_account());
 		let liquidity_limit = CircuitBreaker::add_liquidity_limit_per_asset(CORE_ASSET_ID).unwrap();
@@ -147,18 +167,7 @@ fn add_liquidity_to_omnipool_should_work_when_liquidity_limit_per_block_not_exce
 fn add_liquidity_to_omnipool_should_fail_when_liquidity_limit_per_block_exceeded() {
 	Hydra::execute_with(|| {
 		//Arrange
-		assert_ok!(hydradx_runtime::Omnipool::set_tvl_cap(
-			hydradx_runtime::Origin::root(),
-			222_222_000_000_000_000_000_000,
-		));
-
-		assert_ok!(Omnipool::initialize_pool(
-			RawOrigin::Root.into(),
-			FixedU128::from_float(0.65),
-			FixedU128::from(1),
-			Permill::from_percent(100),
-			Permill::from_percent(100)
-		));
+		init_omnipool();
 
 		let hdx_balance_in_omnipool = Balances::free_balance(&Omnipool::protocol_account());
 		let liquidity_limit = CircuitBreaker::add_liquidity_limit_per_asset(CORE_ASSET_ID).unwrap();
@@ -184,4 +193,19 @@ fn add_liquidity_to_omnipool_should_fail_when_liquidity_limit_per_block_exceeded
 			pallet_circuit_breaker::Error::<hydradx_runtime::Runtime>::MaxLiquidityLimitPerBlockReached
 		);
 	});
+}
+
+fn init_omnipool() {
+	assert_ok!(hydradx_runtime::Omnipool::set_tvl_cap(
+		hydradx_runtime::Origin::root(),
+		222_222_000_000_000_000_000_000,
+	));
+
+	assert_ok!(Omnipool::initialize_pool(
+		RawOrigin::Root.into(),
+		FixedU128::from_float(0.00001), // adjust the amount of LRNA to roughly match the amount of LRNA that belongs to HDX. This way we can avoid MaxOutRatioExceeded error.
+		FixedU128::from(1),
+		Permill::from_percent(100),
+		Permill::from_percent(100)
+	));
 }
