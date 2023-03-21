fc99cd14801581a740afa584f3b1525e6bc2d8ee
dmoka <mokabme@gmail.com>
2023-03-21 11:20:17 +0100

    fix test for sell triggering trade limit error

--- integration-tests/src/circuit_breaker.rs
+++ integration-tests/src/circuit_breaker.rs
@@ -97,22 +97,27 @@ fn sell_lrna_in_omnipool_should_fail_when_min_trade_limit_per_block_exceeded() {
 		//Arrange
 		init_omnipool();
 
-		let lrna_balance_in_omnipool = Tokens::free_balance(LRNA, &Omnipool::protocol_account());
-		let trade_volume_limit = CircuitBreaker::trade_volume_limit_per_asset(LRNA);
-		let sell_amount = CircuitBreaker::calculate_limit(lrna_balance_in_omnipool, trade_volume_limit)
-			.unwrap()
-			.checked_add(1)
-			.unwrap();
-
 		assert_ok!(Tokens::set_balance(
 			RawOrigin::Root.into(),
 			ALICE.into(),
 			LRNA,
-			sell_amount,
+			1000000000000 * UNITS,
 			0,
 		));
 
 		let min_limit = 0;
+		let sell_amount = 300000 * UNITS;
+
+		//We need to split in multiple sells to avoid max in ratio error
+		for _ in 1..=3 {
+			assert_ok!(Omnipool::sell(
+				hydradx_runtime::Origin::signed(ALICE.into()),
+				LRNA,
+				CORE_ASSET_ID,
+				sell_amount,
+				min_limit
+			));
+		}
 
 		//Act and assert
 		assert_noop!(
