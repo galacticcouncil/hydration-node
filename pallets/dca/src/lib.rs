3ee5d50b634b35bd7f20779eb24c3a3469f680e7
dmoka <mokabme@gmail.com>
2023-05-04 15:53:39 +0200

    moved cfg attibute to doc comment to keep it consistent with other pallets

--- pallets/dca/src/lib.rs
+++ pallets/dca/src/lib.rs
@@ -14,7 +14,6 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-#![cfg_attr(not(feature = "std"), no_std)]
 
 //! # DCA pallet
 //!
@@ -30,6 +29,8 @@
 //! Orders are executed on block initialize and they are sorted based on randomness derived from relay chain block number.
 //! Therefore they cannot be front-ran in the block they are executed.
 //!
+
+#![cfg_attr(not(feature = "std"), no_std)]
 
 use codec::MaxEncodedLen;
 use frame_support::{
