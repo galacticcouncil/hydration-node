5bf72a7f2a66de4627799d182d908b2a4ac1b384
Roznovjak <richardroznovjak@gmail.com>
2023-05-09 16:01:51 +0200

    bump runtime versions

--- runtime/testing-hydradx/src/lib.rs
+++ runtime/testing-hydradx/src/lib.rs
@@ -111,7 +111,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("testing-hydradx"),
 	impl_name: create_runtime_str!("testing-hydradx"),
 	authoring_version: 1,
-	spec_version: 145,
+	spec_version: 146,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
