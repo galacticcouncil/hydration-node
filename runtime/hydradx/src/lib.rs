c0e4ff464ffae7fc4e393ca9cbcbb1380a886178
Roznovjak <richardroznovjak@gmail.com>
2023-05-12 12:11:39 +0200

    bump crate versions

--- runtime/hydradx/src/lib.rs
+++ runtime/hydradx/src/lib.rs
@@ -101,7 +101,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("hydradx"),
 	impl_name: create_runtime_str!("hydradx"),
 	authoring_version: 1,
-	spec_version: 148,
+	spec_version: 149,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
