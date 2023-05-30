8113f131ee927b94ca5e702b9615b20913f9d7ee
Alexander Popiak <alexander.popiak@parity.io>
2023-05-30 12:26:48 +0200

    add xcm rate limit migration and update warehouse

--- runtime/hydradx/src/lib.rs
+++ runtime/hydradx/src/lib.rs
@@ -1108,6 +1108,7 @@ pub type Executive = frame_executive::Executive<
 		ParachainSystem,
 		migrations::OnRuntimeUpgradeMigration,
 		migrations::MigrateRegistryLocationToV3<Runtime>,
+		migrations::XcmRateLimitMigration,
 	),
 >;
 
