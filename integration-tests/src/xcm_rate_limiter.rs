30f171faeafddc04bb1ed3b36d552de1ba85e455
Alexander Popiak <alexander.popiak@parity.io>
2023-06-26 11:12:19 +0200

    add test for executing deferred messages

--- integration-tests/src/xcm_rate_limiter.rs
+++ integration-tests/src/xcm_rate_limiter.rs
@@ -156,3 +156,97 @@ fn xcm_rate_limiter_should_not_limit_aca_when_limit_is_not_exceeded() {
 		);
 	});
 }
+
+#[test]
+fn deferred_messages_should_be_executable_by_root() {
+	// Arrange
+	TestNet::reset();
+
+	Hydra::execute_with(|| {
+		assert_ok!(hydradx_runtime::AssetRegistry::set_location(
+			hydradx_runtime::RuntimeOrigin::root(),
+			ACA,
+			hydradx_runtime::AssetLocation(MultiLocation::new(1, X2(Parachain(ACALA_PARA_ID), GeneralIndex(0))))
+		));
+
+		// set an xcm rate limit
+		assert_ok!(hydradx_runtime::AssetRegistry::update(
+			hydradx_runtime::RuntimeOrigin::root(),
+			ACA,
+			b"ACA".to_vec(),
+			AssetType::Token,
+			None,
+			Some(50 * UNITS),
+		));
+
+		assert_eq!(hydradx_runtime::Tokens::free_balance(ACA, &AccountId::from(BOB)), 0);
+	});
+
+	let amount = 100 * UNITS;
+	let mut message_hash = None;
+	let max_weight = Weight::from_ref_time(399_600_000_000);
+	Acala::execute_with(|| {
+		// Act
+		assert_ok!(hydradx_runtime::XTokens::transfer(
+			hydradx_runtime::RuntimeOrigin::signed(ALICE.into()),
+			0,
+			amount,
+			Box::new(
+				MultiLocation::new(
+					1,
+					X2(
+						Junction::Parachain(HYDRA_PARA_ID),
+						Junction::AccountId32 { id: BOB, network: None }
+					)
+				)
+				.into()
+			),
+			WeightLimit::Limited(max_weight),
+		));
+
+		message_hash = get_message_hash_from_event(2);
+
+		// Assert
+		assert_eq!(
+			hydradx_runtime::Balances::free_balance(&AccountId::from(ALICE)),
+			ALICE_INITIAL_NATIVE_BALANCE - amount
+		);
+	});
+
+	Hydra::execute_with(|| {
+		expect_hydra_events(vec![
+			cumulus_pallet_xcmp_queue::Event::XcmDeferred {
+				sender: ACALA_PARA_ID.into(),
+				sent_at: 3,
+				deferred_to: 604, // received at 4 plus 600 blocks of deferral
+				message_hash,
+			}
+			.into(),
+			pallet_relaychain_info::Event::CurrentBlockNumbers {
+				parachain_block_number: 1,
+				relaychain_block_number: 5,
+			}
+			.into(),
+		]);
+		assert_eq!(hydradx_runtime::Tokens::free_balance(ACA, &AccountId::from(BOB)), 0);
+
+		set_relaychain_block_number(604);
+
+		assert_eq!(hydradx_runtime::Tokens::free_balance(ACA, &AccountId::from(BOB)), 0);
+		assert_ok!(hydradx_runtime::XcmpQueue::service_deferred(
+			hydradx_runtime::RuntimeOrigin::root(),
+			max_weight,
+			ACALA_PARA_ID.into(),
+		));
+
+		let fee = 400641025641;
+		assert_eq!(
+			hydradx_runtime::Tokens::free_balance(ACA, &AccountId::from(BOB)),
+			amount - fee
+		);
+		assert_eq!(
+			hydradx_runtime::Tokens::free_balance(ACA, &hydradx_runtime::Treasury::account_id()),
+			fee // fees should go to treasury
+		);
+	});
+}
