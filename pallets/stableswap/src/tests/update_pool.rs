6bebf2a4d9d0db3b15af4bb2503be74d304c387d
Martin <martin.hloska@gmail.com>
2023-07-07 09:01:33 +0200

    fix: check if pool and asset exists when setting tradable state

--- pallets/stableswap/src/tests/update_pool.rs
+++ pallets/stableswap/src/tests/update_pool.rs
@@ -1,6 +1,6 @@
 use crate::tests::mock::*;
-use crate::types::PoolInfo;
-use crate::{Error, Pools};
+use crate::types::{PoolInfo, Tradability};
+use crate::{AssetTradability, Error, Pools};
 use frame_support::{assert_noop, assert_ok};
 use sp_runtime::Permill;
 use std::num::NonZeroU16;
@@ -240,5 +240,89 @@ fn update_pool_should_fail_when_pool_does_not_exists() {
 				Stableswap::update_pool_fees(RuntimeOrigin::root(), pool_id, Some(Permill::from_percent(1)), None),
 				Error::<Test>::PoolNotFound
 			);
+		});
+}
+
+#[test]
+fn set_tradable_state_should_work_when_asset_in_pool() {
+	let asset_a: AssetId = 1;
+	let asset_b: AssetId = 2;
+	let pool_id: AssetId = 100;
+
+	ExtBuilder::default()
+		.with_endowed_accounts(vec![(ALICE, asset_a, 200 * ONE), (ALICE, asset_b, 200 * ONE)])
+		.with_registered_asset("pool".as_bytes().to_vec(), pool_id)
+		.with_registered_asset("one".as_bytes().to_vec(), asset_a)
+		.with_registered_asset("two".as_bytes().to_vec(), asset_b)
+		.build()
+		.execute_with(|| {
+			assert_ok!(Stableswap::create_pool(
+				RuntimeOrigin::root(),
+				pool_id,
+				vec![asset_a, asset_b],
+				100,
+				Permill::from_percent(0),
+				Permill::from_percent(0),
+			));
+
+			assert_ok!(Stableswap::set_asset_tradable_state(
+				RuntimeOrigin::root(),
+				pool_id,
+				asset_a,
+				Tradability::FROZEN,
+			));
+
+			assert_eq!(
+				<AssetTradability<Test>>::get(pool_id, asset_a),
+				Tradability::FROZEN,
+			);
+		});
+}
+#[test]
+fn set_tradable_state_should_fail_when_asset_not_in_pool() {
+	let asset_a: AssetId = 1;
+	let asset_b: AssetId = 2;
+	let pool_id: AssetId = 100;
+
+	ExtBuilder::default()
+		.with_endowed_accounts(vec![(ALICE, asset_a, 200 * ONE), (ALICE, asset_b, 200 * ONE)])
+		.with_registered_asset("pool".as_bytes().to_vec(), pool_id)
+		.with_registered_asset("one".as_bytes().to_vec(), asset_a)
+		.with_registered_asset("two".as_bytes().to_vec(), asset_b)
+		.build()
+		.execute_with(|| {
+			assert_ok!(Stableswap::create_pool(
+				RuntimeOrigin::root(),
+				pool_id,
+				vec![asset_a, asset_b],
+				100,
+				Permill::from_percent(0),
+				Permill::from_percent(0),
+			));
+
+			assert_noop!(Stableswap::set_asset_tradable_state(
+				RuntimeOrigin::root(),
+				pool_id,
+				3,
+				Tradability::FROZEN,
+			),
+			Error::<Test>::AssetNotInPool);
+		});
+}
+#[test]
+fn set_tradable_state_should_fail_when_pool_does_not_exist() {
+	let pool_id: AssetId = 100;
+
+	ExtBuilder::default()
+		.with_registered_asset("pool".as_bytes().to_vec(), pool_id)
+		.build()
+		.execute_with(|| {
+			assert_noop!(Stableswap::set_asset_tradable_state(
+				RuntimeOrigin::root(),
+				pool_id,
+				1,
+				Tradability::FROZEN,
+			),
+			Error::<Test>::PoolNotFound);
 		});
 }
