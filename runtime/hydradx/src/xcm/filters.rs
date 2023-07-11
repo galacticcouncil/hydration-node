c372eaaaa344c444c412128f135675504085489e
Alexander Popiak <alexander.popiak@parity.io>
2023-07-11 15:32:16 +0200

    formatting

--- runtime/hydradx/src/xcm/filters.rs
+++ runtime/hydradx/src/xcm/filters.rs
@@ -8,22 +8,28 @@ pub struct AllowTransferAndSwap<RuntimeCall>(PhantomData<RuntimeCall>);
 
 fn allowed_or_recurse<RuntimeCall>(inst: &Instruction<RuntimeCall>) -> Either<bool, &Xcm<()>> {
 	match inst {
-		ClearOrigin |
-		ClaimAsset {..} |
-		ExchangeAsset { .. } |
-		WithdrawAsset (..) |
-		TransferAsset { .. } |
-		DepositAsset { .. } |
-		SetTopic(..) | ClearTopic |
-		ExpectAsset(..) | BurnAsset(..) |
-		BuyExecution  { .. } => Either::Left(true),
-		InitiateReserveWithdraw { xcm, .. } | DepositReserveAsset { xcm, .. } | TransferReserveAsset { xcm, .. } => Either::Right(xcm),
+		ClearOrigin
+		| ClaimAsset { .. }
+		| ExchangeAsset { .. }
+		| WithdrawAsset(..)
+		| TransferAsset { .. }
+		| DepositAsset { .. }
+		| SetTopic(..)
+		| ClearTopic
+		| ExpectAsset(..)
+		| BurnAsset(..)
+		| BuyExecution { .. } => Either::Left(true),
+		InitiateReserveWithdraw { xcm, .. } | DepositReserveAsset { xcm, .. } | TransferReserveAsset { xcm, .. } => {
+			Either::Right(xcm)
+		}
 		_ => Either::Left(false),
 	}
 }
 
 fn check_instructions_recursively<RuntimeCall>(xcm: &Xcm<RuntimeCall>, depth: u16) -> bool {
-	if depth > 6 { return false } // TODO: make configurable?
+	if depth > 6 {
+		return false;
+	} // TODO: make configurable?
 	let limit = 10; // TODO: make configurable?
 	let count = Cell::new(0usize);
 	let mut iter = xcm.inner().iter();
@@ -32,10 +38,12 @@ fn check_instructions_recursively<RuntimeCall>(xcm: &Xcm<RuntimeCall>, depth: u1
 		match allowed_or_recurse(inst) {
 			Either::Left(true) => continue,
 			Either::Left(false) => return false,
-			Either::Right(xcm) => if check_instructions_recursively(xcm, depth + 1) {
-				continue
-			} else {
-				return false
+			Either::Right(xcm) => {
+				if check_instructions_recursively(xcm, depth + 1) {
+					continue;
+				} else {
+					return false;
+				}
 			}
 		}
 	}
@@ -62,12 +70,18 @@ mod tests {
 	#[test]
 	fn allow_transfer_and_swap_should_filter_transact() {
 		let call = crate::RuntimeCall::System(frame_system::Call::remark { remark: Vec::new() }).encode();
-		let xcm = Xcm(vec![Transact { origin_kind: OriginKind::Native, require_weight_at_most: Weight::from_parts(1, 1), call: call.into() }]);
-		let loc = MultiLocation::new(0, AccountId32 {
-			network: None,
-			id: [0; 32].into()
-		});
-		assert_eq!(AllowTransferAndSwap::<crate::RuntimeCall>::contains(&(loc, xcm)),
-		false);
+		let xcm = Xcm(vec![Transact {
+			origin_kind: OriginKind::Native,
+			require_weight_at_most: Weight::from_parts(1, 1),
+			call: call.into(),
+		}]);
+		let loc = MultiLocation::new(
+			0,
+			AccountId32 {
+				network: None,
+				id: [0; 32].into(),
+			},
+		);
+		assert_eq!(AllowTransferAndSwap::<crate::RuntimeCall>::contains(&(loc, xcm)), false);
 	}
 }
