69d67a3a32eef2a6b88a50e2449a929f22786b6f
Roznovjak <richardroznovjak@gmail.com>
2023-07-15 19:27:44 +0200

    remove `without_storage_info` macro and add migration

---
+++ pallets/genesis-history/src/migration.rs
@@ -0,0 +1,72 @@
+// This file is part of pallet-genesis-history
+
+// Copyright (C) 2020-2023  Intergalactic, Limited (GIB).
+// SPDX-License-Identifier: Apache-2.0
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License..
+
+pub use crate::*;
+pub use frame_support::{
+	log,
+	traits::{Get, StorageVersion},
+	weights::Weight,
+};
+
+// This migration fixes the corrupted value in the storage and fixes it
+pub mod v1 {
+	use super::*;
+	use hex_literal::hex;
+
+	pub fn pre_migrate<T: Config>() {
+		assert_eq!(StorageVersion::get::<Pallet<T>>(), 0, "Storage version too high.");
+
+		log::info!(
+			target: "runtime::genesis-history",
+			"Genesis history migration: PRE checks successful!"
+		);
+	}
+
+	pub fn migrate<T: Config>() -> Weight {
+		log::info!(
+			target: "runtime::genesis-history",
+			"Running migration to v1 for Genesis history"
+		);
+
+		PreviousChain::<T>::put(Chain {
+			// genesis hash was generated using the following command: hydradx export-genesis-state | sha256sum
+			genesis_hash: H256::from(hex!("5a103904d5d4ca65c1ff40f59d45266ac3fd23725beae623bbdd6f9c5a78883c")),
+			last_block_hash: H256::from(hex!("1c83220b0d0c0c252dddd6a98c9b643e34625419b646c4a6447583c92c01dbcc")),
+		});
+
+		StorageVersion::new(1).put::<Pallet<T>>();
+
+		T::DbWeight::get().reads_writes(0, 2)
+	}
+
+	pub fn post_migrate<T: Config>() {
+		assert_eq!(StorageVersion::get::<Pallet<T>>(), 1, "Unexpected storage version.");
+		assert_eq!(
+			PreviousChain::<T>::get(),
+			Chain {
+				genesis_hash: H256::from(hex!("1c83220b0d0c0c252dddd6a98c9b643e34625419b646c4a6447583c92c01dbcc")),
+				last_block_hash: H256::from(hex!("1c83220b0d0c0c252dddd6a98c9b643e34625419b646c4a6447583c92c01dbcc")),
+			},
+			"Unexpected storage version."
+		);
+
+		log::info!(
+			target: "runtime::genesis-history",
+			"Genesis history migration: POST checks successful!"
+		);
+	}
+}
