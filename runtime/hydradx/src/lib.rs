e0b70e5d7c8ca2002afdfe4db581ffbc9f2de08c
mrq <mrq@mrq.cz>
2023-09-04 21:40:53 +0200

    runtime 176

--- runtime/hydradx/src/lib.rs
+++ runtime/hydradx/src/lib.rs
@@ -94,7 +94,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("hydradx"),
 	impl_name: create_runtime_str!("hydradx"),
 	authoring_version: 1,
-	spec_version: 175,
+	spec_version: 176,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
