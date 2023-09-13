dcf8de822832519b30ce14e89b9787e35c24c05c
dmoka <mokabme@gmail.com>
2023-09-13 12:34:17 +0200

    add back missing hex dependency

--- runtime/hydradx/src/evm/accounts_conversion.rs
+++ runtime/hydradx/src/evm/accounts_conversion.rs
@@ -25,6 +25,7 @@ use crate::{
 };
 use codec::{Decode, Encode};
 use core::marker::PhantomData;
+use hex_literal::hex;
 use pallet_evm::AddressMapping;
 use sp_core::{crypto::ByteArray, H160};
 use sp_runtime::traits::AccountIdConversion;
