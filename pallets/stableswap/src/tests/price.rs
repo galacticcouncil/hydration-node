cfcf75d6b3e387245a826614fc06814b1a7d0b69
Martin Hloska <martin.hloska@gmail.com>
2023-09-29 15:16:09 +0200

    add trade spot price tests

--- pallets/stableswap/src/tests/price.rs
+++ pallets/stableswap/src/tests/price.rs
@@ -1,5 +1,5 @@
 use crate::tests::mock::*;
-use crate::types::{AssetAmount, PoolInfo, PoolState};
+use crate::types::{AssetAmount, PoolInfo};
 use crate::*;
 use frame_support::assert_ok;
 use sp_runtime::{FixedU128, Permill};
@@ -16,8 +16,19 @@ fn get_share_price(pool_id: AssetId) -> FixedU128 {
 	FixedU128::from_rational(share_price.0, share_price.1)
 }
 
+fn asset_spot_price(pool_id: AssetId, asset_id: AssetId) -> FixedU128 {
+	let pool_account = pool_account(pool_id);
+	let pool = <Pools<Test>>::get(pool_id).unwrap();
+	let balances = pool.balances::<Test>(&pool_account).unwrap();
+	let amp = Pallet::<Test>::get_amplification(&pool);
+	let asset_idx = pool.find_asset(asset_id).unwrap();
+	let d = hydra_dx_math::stableswap::calculate_d::<D_ITERATIONS>(&balances, amp).unwrap();
+	let p = hydra_dx_math::stableswap::calculate_spot_price(&balances, amp, d, asset_idx).unwrap();
+	FixedU128::from_rational(p.0, p.1)
+}
+
 #[test]
-fn test_share_price_in_trades() {
+fn test_spot_price_in_sell() {
 	let asset_a: AssetId = 1;
 	let asset_b: AssetId = 2;
 	let asset_c: AssetId = 3;
@@ -56,20 +67,94 @@ fn test_share_price_in_trades() {
 			let pool_id = get_pool_id_at(0);
 			Tokens::withdraw(pool_id, &ALICE, 5906657405945079804575283).unwrap();
 
-			let p0 = get_share_price(pool_id);
-			dbg!(p0);
-			// ACT
+			let amount = 1_000_000_000_000_000_000;
+
+			let initial_spot_price = asset_spot_price(pool_id, asset_b);
 			assert_ok!(Stableswap::sell(
 				RuntimeOrigin::signed(BOB),
 				pool_id,
 				asset_a,
 				asset_b,
-				1_000_000_000_000_000_000,
+				amount,
 				0,
 			));
 
-			let p = get_share_price(pool_id);
-			dbg!(p);
+			let received = Tokens::free_balance(asset_b, &BOB);
+			let exec_price = FixedU128::from_rational(amount, received);
+			let exec_price = exec_price / FixedU128::from(1_000_000_000_000);
+			assert!(exec_price >= initial_spot_price);
+
+			let final_spot_price = asset_spot_price(pool_id, asset_b);
+			assert!(exec_price <= final_spot_price);
+		});
+}
+
+#[test]
+fn test_spot_price_in_buy() {
+	let asset_a: AssetId = 1;
+	let asset_b: AssetId = 2;
+	let asset_c: AssetId = 3;
+
+	ExtBuilder::default()
+		.with_endowed_accounts(vec![
+			(BOB, asset_a, 2_000_000_000_000_000_000),
+			(ALICE, asset_a, 52425995641788588073263117),
+			(ALICE, asset_b, 52033213790329),
+			(ALICE, asset_c, 119135337044269),
+		])
+		.with_registered_asset("one".as_bytes().to_vec(), asset_a, 18)
+		.with_registered_asset("two".as_bytes().to_vec(), asset_b, 6)
+		.with_registered_asset("three".as_bytes().to_vec(), asset_c, 6)
+		.with_pool(
+			ALICE,
+			PoolInfo::<AssetId, u64> {
+				assets: vec![asset_a, asset_b, asset_c].try_into().unwrap(),
+				initial_amplification: NonZeroU16::new(2000).unwrap(),
+				final_amplification: NonZeroU16::new(2000).unwrap(),
+				initial_block: 0,
+				final_block: 0,
+				fee: Permill::zero(),
+			},
+			InitialLiquidity {
+				account: ALICE,
+				assets: vec![
+					AssetAmount::new(asset_a, 52425995641788588073263117),
+					AssetAmount::new(asset_b, 52033213790329),
+					AssetAmount::new(asset_c, 119135337044269),
+				],
+			},
+		)
+		.build()
+		.execute_with(|| {
+			let pool_id = get_pool_id_at(0);
+			Tokens::withdraw(pool_id, &ALICE, 5906657405945079804575283).unwrap();
+
+			let amount = 1_000_000;
+
+			let initial_spot_price = asset_spot_price(pool_id, asset_b);
+			assert_ok!(Stableswap::buy(
+				RuntimeOrigin::signed(BOB),
+				pool_id,
+				asset_b,
+				asset_a,
+				amount,
+				u128::MAX,
+			));
+
+			let a_balance = Tokens::free_balance(asset_a, &BOB);
+			let delta_a = 2_000_000_000_000_000_000 - a_balance;
+			dbg!(delta_a);
+			let exec_price = FixedU128::from_rational(delta_a, amount);
+			let exec_price = exec_price / FixedU128::from(1_000_000_000_000);
+			dbg!(exec_price);
+			assert!(exec_price >= initial_spot_price);
+
+			let final_spot_price = asset_spot_price(pool_id, asset_b);
+			let i = initial_spot_price;
+			let e = exec_price;
+			let f = final_spot_price;
+			dbg!(i, e, f);
+			assert!(exec_price <= final_spot_price);
 		});
 }
 
@@ -116,7 +201,6 @@ fn test_share_price_in_add_remove_liquidity() {
 			let pool_account = pool_account(pool_id);
 			let amount = 1_000_000_000_000_000_000;
 			let share_price_initial = get_share_price(pool_id);
-			dbg!(share_price_initial);
 			let initial_shares = Tokens::total_issuance(&pool_id);
 			assert_ok!(Stableswap::add_liquidity(
 				RuntimeOrigin::signed(BOB),
@@ -128,15 +212,10 @@ fn test_share_price_in_add_remove_liquidity() {
 			let delta_s = final_shares - initial_shares;
 			let exec_price = FixedU128::from_rational(amount, delta_s);
 
-			if share_price_initial > exec_price {
-				let acceptable = FixedU128::from_rational(10, 10_000);
-				let diff = (share_price_initial - exec_price) / share_price_initial;
-				assert!(diff <= acceptable);
-			}
+			assert!(share_price_initial <= exec_price);
 
 			// Remove liquidity
 			let share_price_initial = get_share_price(pool_id);
-			dbg!(share_price_initial);
 			let a_initial = Tokens::free_balance(asset_a, &pool_account);
 			assert_ok!(Stableswap::remove_liquidity_one_asset(
 				RuntimeOrigin::signed(BOB),
@@ -147,11 +226,8 @@ fn test_share_price_in_add_remove_liquidity() {
 			));
 			let a_final = Tokens::free_balance(asset_a, &pool_account);
 			let delta_a = a_initial - a_final;
-			dbg!(delta_a);
-
 			let exec_price = FixedU128::from_rational(delta_a, delta_s);
-			dbg!(exec_price);
-			assert!(share_price_initial > exec_price);
+			assert!(share_price_initial >= exec_price);
 		});
 }
 
@@ -196,11 +272,10 @@ fn test_share_price_in_add_shares_remove_liquidity() {
 			Tokens::withdraw(pool_id, &ALICE, 5906657405945079804575283).unwrap();
 
 			let pool_account = pool_account(pool_id);
-			let amount = 1_000_000_000_000_000_000;
 			let share_price_initial = get_share_price(pool_id);
-			dbg!(share_price_initial);
 			let initial_shares = Tokens::total_issuance(&pool_id);
 			let desired_shares = 973798810707557758;
+			let intial_a = Tokens::free_balance(asset_a, &BOB);
 			assert_ok!(Stableswap::add_liquidity_shares(
 				RuntimeOrigin::signed(BOB),
 				pool_id,
@@ -209,20 +284,16 @@ fn test_share_price_in_add_shares_remove_liquidity() {
 				1_100_000_000_000_000_000,
 			));
 
+			let final_a = Tokens::free_balance(asset_a, &BOB);
+			let delta_a = intial_a - final_a;
 			let final_shares = Tokens::total_issuance(&pool_id);
 			let delta_s = final_shares - initial_shares;
 			assert_eq!(delta_s, desired_shares);
-			let exec_price = FixedU128::from_rational(amount, delta_s);
-
-			if share_price_initial > exec_price {
-				let acceptable = FixedU128::from_rational(10, 10_000);
-				let diff = (share_price_initial - exec_price) / share_price_initial;
-				assert!(diff <= acceptable);
-			}
+			let exec_price = FixedU128::from_rational(delta_a, delta_s);
+			assert!(share_price_initial <= exec_price);
 
 			// Remove liquidity
 			let share_price_initial = get_share_price(pool_id);
-			dbg!(share_price_initial);
 			let a_initial = Tokens::free_balance(asset_a, &pool_account);
 			assert_ok!(Stableswap::remove_liquidity_one_asset(
 				RuntimeOrigin::signed(BOB),
@@ -233,12 +304,8 @@ fn test_share_price_in_add_shares_remove_liquidity() {
 			));
 			let a_final = Tokens::free_balance(asset_a, &pool_account);
 			let delta_a = a_initial - a_final;
-			dbg!(delta_a);
-
 			let exec_price = FixedU128::from_rational(delta_a, delta_s);
-			dbg!(exec_price);
-
-			assert!(share_price_initial > exec_price);
+			assert!(share_price_initial >= exec_price);
 		});
 }
 
@@ -246,7 +313,6 @@ fn test_share_price_in_add_shares_remove_liquidity() {
 fn test_share_price_case() {
 	let asset_a: AssetId = 1;
 	let asset_b: AssetId = 2;
-	let asset_c: AssetId = 3;
 
 	ExtBuilder::default()
 		.with_endowed_accounts(vec![
@@ -290,12 +356,11 @@ fn test_share_price_case() {
 			let final_shares = Tokens::total_issuance(&pool_id);
 			let delta_s = final_shares - initial_shares;
 			let exec_price = FixedU128::from_rational(amount, delta_s);
-			assert!(share_price_initial < exec_price);
+			assert!(share_price_initial <= exec_price);
 
 			// Remove liquidity
 			let share_price_initial = get_share_price(pool_id);
 			let a_initial = Tokens::free_balance(asset_a, &pool_account);
-
 			assert_ok!(Stableswap::remove_liquidity_one_asset(
 				RuntimeOrigin::signed(BOB),
 				pool_id,
@@ -305,10 +370,7 @@ fn test_share_price_case() {
 			));
 			let a_final = Tokens::free_balance(asset_a, &pool_account);
 			let delta_a = a_initial - a_final;
-			dbg!(delta_a);
-
 			let exec_price = FixedU128::from_rational(delta_a, delta_s);
-			dbg!(exec_price);
-			assert!(share_price_initial > exec_price);
+			assert!(share_price_initial >= exec_price);
 		});
 }
