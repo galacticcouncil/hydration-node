746b3653ec19fac8166ac68ebc9a06f713c006f8
Roznovjak <richardroznovjak@gmail.com>
2023-11-06 11:23:30 +0100

    formatting

--- runtime/hydradx/src/migrations.rs
+++ runtime/hydradx/src/migrations.rs
@@ -17,7 +17,8 @@ impl OnRuntimeUpgrade for OnRuntimeUpgradeMigration {
 		let mut weight: Weight = Weight::zero();
 
 		log::info!("Migrate Collator Selection Pallet to v1 start");
-		weight = weight.saturating_add(pallet_collator_selection::migration::v1::MigrateToV1::<Runtime>::on_runtime_upgrade());
+		weight = weight
+			.saturating_add(pallet_collator_selection::migration::v1::MigrateToV1::<Runtime>::on_runtime_upgrade());
 		log::info!("Migrate Collator Selection Pallet to v1 end");
 
 		log::info!("Migrate Unknown Tokens Pallet to v2 start");
@@ -25,7 +26,8 @@ impl OnRuntimeUpgrade for OnRuntimeUpgradeMigration {
 		log::info!("Migrate Unknown Tokens Pallet to v2 end");
 
 		log::info!("Migrate XCM Pallet to v1 start");
-		weight = weight.saturating_add(pallet_xcm::migration::v1::VersionUncheckedMigrateToV1::<Runtime>::on_runtime_upgrade());
+		weight = weight
+			.saturating_add(pallet_xcm::migration::v1::VersionUncheckedMigrateToV1::<Runtime>::on_runtime_upgrade());
 		log::info!("Migrate XCM Pallet to v1 end");
 
 		weight
