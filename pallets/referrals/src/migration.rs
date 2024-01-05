273a3298ed0184e081d4f18a98200afdc8fe4258
Martin Hloska <martin.hloska@gmail.com>
2024-01-05 20:56:57 +0100

    add codes and accounts

--- pallets/referrals/src/migration.rs
+++ pallets/referrals/src/migration.rs
@@ -18,7 +18,20 @@ use frame_support::{traits::Get, weights::Weight};
 use sp_core::crypto::AccountId32;
 use sp_runtime::traits::{IdentifyAccount, Verify};
 
-const PARACHAIN_CODES: [(&str, &str); 1] = [("BALLS", "Account")];
+pub const PARACHAIN_CODES: [(&str, &str); 12] = [
+	("MOONBEAM", "7LCt6dFmtiRrwZv2YyEgQWW3GxsGX3Krmgzv9Xj7GQ9tG2j8"),
+	("ASSETHUB", "7LCt6dFqtxzdKVB2648jWW9d85doiFfLSbZJDNAMVJNxh5rJ"),
+	("INTERLAY", "7LCt6dFsW7xwUutdYad3oeQ1zfQvZ9THXbBupWLqpd72bmnM"),
+	("CENTRIFUGE", "7LCt6dFsJVukxnxpix9KcTkwu2kWQnXARsy6BuBHEL54NcS6"),
+	("ASTAR", "7LCt6dFnHxYDyomeCEC8nsnBUEC6omC6y7SZQk4ESzDpiDYo"),
+	("BIFROST", "7LCt6dFs6sraSg31uKfbRH7soQ66GRb3LAkGZJ1ie3369crq"),
+	("ZEITGEIST", "7LCt6dFCEKr7CctCKBb6CcQdV9iHDue3JcpxkkFCqJZbk3Xk"),
+	("PHALA", "7LCt6dFt6z8V3Gg41U4EPCKEHZQAzEFepirNiKqXbWCwHECN"),
+	("UNIQUE", "7LCt6dFtWEEr5WXfej1gmZbNUpj1Gx7u29J1yYAen6GsjQTj"),
+	("NODLE", "7LCt6dFrJPdrNCKncokgeYZbQsSRgyrYwKrz2sMUGruDF9gJ"),
+	("SUBSOCIAL", "7LCt6dFE2vLjshEThqtdwGAGMqg2XA39C1pMSCjG9wsKnR2Q"),
+	("POLKADOT", "7KQx4f7yU3hqZHfvDVnSfe6mpgAT8Pxyr67LXHV6nsbZo3Tm"),
+];
 
 pub fn preregister_parachain_codes<T: Config>() -> Weight
 where
