089d1f09ab7913136ad1e8cf89def6829ed90413
Valery Gantchev <v@lery.nl>
2024-03-01 16:52:00 +0100

    Cargo.lock and delete runtime-mock/main

--- runtime-mock/src/main.rs
+++
@@ -1,7 +0,0 @@
-use runtime_mock::hydradx_mocked_runtime;
-
-fn main() {
-	let path = std::path::PathBuf::from("./MOCK_SNAPSHOT");
-	let ext = hydradx_mocked_runtime();
-	scraper::save_externalities::<hydradx_runtime::Block>(ext, path).unwrap();
-}
