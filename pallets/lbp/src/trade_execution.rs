5628a9db861adb7e6ee1bbcefd4030121935b15b
dmoka <mokabme@gmail.com>
2024-04-15 08:19:46 +0200

    add some explanatory comments

--- pallets/lbp/src/trade_execution.rs
+++ pallets/lbp/src/trade_execution.rs
@@ -178,7 +178,7 @@ impl<T: Config> TradeExecution<T::RuntimeOrigin, T::AccountId, AssetId, Balance>
 		};
 
 		let spot_price_with_fee = if fee_asset == assets.asset_out {
-			//Pool pays fee, but fee is deducted from asset out.
+			//Buyer bears fee, the fee is deducted from asset out.
 			//We divide by (1-f) to reflect correct amount out after the fee deduction
 			let fee = FixedU128::checked_from_rational(fee.0, fee.1).ok_or(ExecutorError::Error(Corruption))?;
 			let fee_multiplier = FixedU128::from_rational(1, 1)
@@ -189,6 +189,7 @@ impl<T: Config> TradeExecution<T::RuntimeOrigin, T::AccountId, AssetId, Balance>
 				.checked_div(&fee_multiplier)
 				.ok_or(ExecutorError::Error(Corruption))?
 		} else {
+			//Pool bears repay fee
 			//Fee does not change spot price as user receives the whole amount out based on whole amount in.
 			//For the trade, amount_in-fee is transferred from the user, then in the end the fee is transferred to fee-collector
 			spot_price_without_fee
