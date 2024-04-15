1c215498d729c4ccee8625ea7e466c9fb123d80e
Roznovjak <richardroznovjak@gmail.com>
2024-04-15 13:43:18 -0500

    rebenchmark AMMs

--- runtime/hydradx/src/benchmarking/xyk.rs
+++ runtime/hydradx/src/benchmarking/xyk.rs
@@ -266,9 +266,6 @@ runtime_benchmarks! {
 	}
 
 	calculate_spot_price {
-		let c in 1..3;	// number of times calculate_buy is executed
-		let e in 0..1;	// if e == 1, execute_buy is executed
-
 		let asset_a = register_external_asset(b"TKNA".to_vec()).map_err(|_| BenchmarkError::Stop("Failed to register asset"))?;
 		let asset_b = register_external_asset(b"TKNB".to_vec()).map_err(|_| BenchmarkError::Stop("Failed to register asset"))?;
 		let fee_asset = register_asset(b"FEE".to_vec(), 1u128).map_err(|_| BenchmarkError::Stop("Failed to register asset"))?;
