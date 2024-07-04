0e220dbd2e64196294c44a36d2523cfcd8819edd
Martin <martin.hloska@gmail.com>
2024-07-04 12:00:08 +0200

    fix integration tests

--- pallets/conviction-voting/src/benchmarking.rs
+++ pallets/conviction-voting/src/benchmarking.rs
@@ -75,7 +75,7 @@ benchmarks_instance_pallet! {
 		whitelist_account!(caller);
 		let account_vote = account_vote::<T, I>(100u32.into());
 
-		T::DemocracyHooks::on_vote_worst_case(&caller);
+		T::VotingHooks::on_vote_worst_case(&caller);
 
 		let (class, all_polls) = fill_voting::<T, I>();
 		let polls = &all_polls[&class];
@@ -104,7 +104,7 @@ benchmarks_instance_pallet! {
 		whitelist_account!(caller);
 		let old_account_vote = account_vote::<T, I>(100u32.into());
 
-		T::DemocracyHooks::on_vote_worst_case(&caller);
+		T::VotingHooks::on_vote_worst_case(&caller);
 
 		let (class, all_polls) = fill_voting::<T, I>();
 		let polls = &all_polls[&class];
@@ -134,7 +134,7 @@ benchmarks_instance_pallet! {
 		whitelist_account!(caller);
 		let old_account_vote = account_vote::<T, I>(100u32.into());
 
-		T::DemocracyHooks::on_remove_vote_worst_case(&caller);
+		T::VotingHooks::on_remove_vote_worst_case(&caller);
 
 		let (class, all_polls) = fill_voting::<T, I>();
 		let polls = &all_polls[&class];
@@ -165,7 +165,7 @@ benchmarks_instance_pallet! {
 		whitelist_account!(caller);
 		let old_account_vote = account_vote::<T, I>(100u32.into());
 
-		T::DemocracyHooks::on_remove_vote_worst_case(&caller);
+		T::VotingHooks::on_remove_vote_worst_case(&caller);
 
 		let (class, all_polls) = fill_voting::<T, I>();
 		let polls = &all_polls[&class];
