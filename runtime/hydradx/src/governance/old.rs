f44b7a962b75e0393bd0b77f22a7cae08cc16ef5
Valery Gantchev <v@lery.nl>
2024-07-30 21:34:04 +0200

    merge conflicts

--- runtime/hydradx/src/governance/old.rs
+++ runtime/hydradx/src/governance/old.rs
@@ -159,6 +159,8 @@ impl pallet_democracy::Config for Runtime {
 	type PalletsOrigin = OriginCaller;
 	type Slash = Treasury;
 	type DemocracyHooks = pallet_staking::integrations::democracy::StakingDemocracy<Runtime>;
+	// Any single technical committee member may remove a vote.
+	type VoteRemovalOrigin = frame_system::EnsureSignedBy<TechCommAccounts, AccountId>;
 }
 
 parameter_types! {
