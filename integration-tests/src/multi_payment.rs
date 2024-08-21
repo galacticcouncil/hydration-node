7a58880a3631f95d38b0d68e37192f9d81440eda
dmoka <mokabme@gmail.com>
2024-08-21 10:07:21 +0200

    formatting

--- integration-tests/src/multi_payment.rs
+++ integration-tests/src/multi_payment.rs
@@ -5,9 +5,9 @@ use frame_support::dispatch::GetDispatchInfo;
 use frame_support::dispatch::PostDispatchInfo;
 use frame_support::storage::with_transaction;
 use frame_support::{assert_noop, assert_ok};
-use hydradx_runtime::{AssetRegistry, TreasuryAccount};
 use hydradx_runtime::Omnipool;
 use hydradx_runtime::DOT_ASSET_LOCATION;
+use hydradx_runtime::{AssetRegistry, TreasuryAccount};
 use hydradx_traits::AssetKind;
 use hydradx_traits::Create;
 use orml_traits::MultiCurrency;
@@ -15,7 +15,7 @@ use pallet_transaction_payment::ChargeTransactionPayment;
 use primitives::constants::currency::UNITS;
 use sp_runtime::traits::SignedExtension;
 use sp_runtime::DispatchResult;
-use sp_runtime::{TransactionOutcome};
+use sp_runtime::TransactionOutcome;
 use xcm_emulator::TestExt;
 
 #[test]
@@ -103,11 +103,18 @@ fn insufficient_asset_can_be_used_as_fee_currency() {
 			let alice_new_insuff_balance = hydradx_runtime::Currencies::free_balance(insufficient_asset, &ALICE.into());
 			assert!(alice_new_insuff_balance < alice_init_insuff_balance);
 
-			let treasury_insuff_balance = hydradx_runtime::Currencies::free_balance(insufficient_asset, &TreasuryAccount::get());
-			assert_eq!(treasury_insuff_balance, 0, "Treasury should not have accumulated insuff asset");
+			let treasury_insuff_balance =
+				hydradx_runtime::Currencies::free_balance(insufficient_asset, &TreasuryAccount::get());
+			assert_eq!(
+				treasury_insuff_balance, 0,
+				"Treasury should not have accumulated insuff asset"
+			);
 
 			let treasury_dot_balance = hydradx_runtime::Currencies::free_balance(DOT, &TreasuryAccount::get());
-			assert!(treasury_dot_balance > 0, "Treasury should have received DOT swapped from insuff asset");
+			assert!(
+				treasury_dot_balance > 0,
+				"Treasury should have received DOT swapped from insuff asset"
+			);
 
 			TransactionOutcome::Commit(DispatchResult::Ok(()))
 		});
@@ -188,7 +195,7 @@ fn sufficient_but_not_accepted_asset_can_be_used_as_fee_currency() {
 				None,
 				None,
 			)
-				.unwrap();
+			.unwrap();
 			create_xyk_pool(sufficient_but_not_accepted_asset, 1000000 * UNITS, DOT, 3000000 * UNITS);
 
 			set_relaychain_block_number(11);
@@ -233,14 +240,22 @@ fn sufficient_but_not_accepted_asset_can_be_used_as_fee_currency() {
 			));
 
 			//Assert
-			let alice_new_suff_balance = hydradx_runtime::Currencies::free_balance(sufficient_but_not_accepted_asset, &ALICE.into());
+			let alice_new_suff_balance =
+				hydradx_runtime::Currencies::free_balance(sufficient_but_not_accepted_asset, &ALICE.into());
 			assert!(alice_new_suff_balance < alice_init_suff_balance);
 
-			let treasury_suff_balance = hydradx_runtime::Currencies::free_balance(sufficient_but_not_accepted_asset, &TreasuryAccount::get());
-			assert_eq!(treasury_suff_balance, 0, "Treasury should not have accumulated non accepted suff asset");
+			let treasury_suff_balance =
+				hydradx_runtime::Currencies::free_balance(sufficient_but_not_accepted_asset, &TreasuryAccount::get());
+			assert_eq!(
+				treasury_suff_balance, 0,
+				"Treasury should not have accumulated non accepted suff asset"
+			);
 
 			let treasury_dot_balance = hydradx_runtime::Currencies::free_balance(DOT, &TreasuryAccount::get());
-			assert!(treasury_dot_balance > 0, "Treasury should have received DOT swapped from non accepted suff asset");
+			assert!(
+				treasury_dot_balance > 0,
+				"Treasury should have received DOT swapped from non accepted suff asset"
+			);
 
 			TransactionOutcome::Commit(DispatchResult::Ok(()))
 		});
