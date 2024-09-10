1c25a4752a3cd3a75f9acc0bd89c8afa9a64ac7e
dmoka <mokabme@gmail.com>
2024-09-10 08:37:16 +0200

    update benchmark of transfer for erc20

--- runtime/hydradx/src/benchmarking/currencies.rs
+++ runtime/hydradx/src/benchmarking/currencies.rs
@@ -1,4 +1,7 @@
-use crate::{AccountId, Amount, AssetId, Balance, Currencies, NativeAssetId, Runtime};
+use std::fs;
+use evm::ExitReason;
+use fp_rpc::runtime_decl_for_ethereum_runtime_rpc_api::EthereumRuntimeRPCApi;
+use crate::{AccountId, Amount, AssetId, Balance, Currencies, EVMAccounts, NativeAssetId, Runtime};
 use primitives::constants::currency::NATIVE_EXISTENTIAL_DEPOSIT;
 
 use sp_std::prelude::*;
@@ -10,10 +13,18 @@ use sp_runtime::SaturatedConversion;
 
 use frame_benchmarking::BenchmarkError;
 use frame_support::assert_ok;
+use hex_literal::hex;
 
 use orml_benchmarking::runtime_benchmarks;
 use orml_traits::MultiCurrency;
 use orml_traits::MultiCurrencyExtended;
+use polkadot_xcm::v3::Junction::AccountKey20;
+use polkadot_xcm::v3::Junctions::X1;
+use polkadot_xcm::v3::MultiLocation;
+use primitive_types::{H160, U256};
+use hydradx_traits::evm::{CallContext, ERC20, EvmAddress, InspectEvmAccounts};
+use crate::evm::Erc20Currency;
+use crate::evm::precompiles::erc20_mapping::{Erc20Mapping, HydraErc20Mapping};
 
 use super::*;
 
@@ -40,8 +51,8 @@ runtime_benchmarks! {
 	transfer_non_native_currency {
 		let amount: Balance = 1_000 * BSX;
 		let from: AccountId = whitelisted_caller();
-		let asset_id = register_asset(b"TST".to_vec(), 1u128).map_err(|_| BenchmarkError::Stop("Failed to register asset"))?;
-		set_balance(asset_id, &from, amount);
+		let contract_address = deploy_token_contract(from.clone());
+		let asset_id = bind_erc20(contract_address);
 
 		let to: AccountId = account("to", 0, SEED);
 		let to_lookup = lookup_of_account(to.clone());
@@ -136,3 +147,79 @@ mod tests {
 
 	impl_benchmark_test_suite!(new_test_ext(),);
 }
+
+//TODO: refactor these to common
+pub fn get_contract_bytecode(name: &str) -> Vec<u8> {
+	let path = format!(
+		"../../scripts/test-contracts/artifacts/contracts/{}.sol/{}.json",
+		name, name
+	);
+	let str = fs::read_to_string(path).unwrap();
+	let json: serde_json::Value = serde_json::from_str(&str).unwrap();
+	let code = json.get("bytecode").unwrap().as_str().unwrap();
+	hex::decode(&code[2..]).unwrap()
+}
+
+pub fn deploy_contract_code(code: Vec<u8>, deployer: EvmAddress) -> EvmAddress {
+	assert_ok!(EVMAccounts::add_contract_deployer(
+		RawOrigin::Root.into(),
+		deployer,
+	));
+
+	let info = crate::Runtime::create(
+		deployer,
+		code.clone(),
+		U256::zero(),
+		U256::from(2000000u64),
+		None,
+		None,
+		None,
+		false,
+		None,
+	);
+
+	let address = match info.clone().unwrap().exit_reason {
+		ExitReason::Succeed(_) => info.unwrap().value,
+		reason => panic!("{:?}", reason),
+	};
+
+	let deployed = crate::Runtime::account_code_at(address.clone());
+	assert_ne!(deployed, vec![0; deployed.len()]);
+	address
+}
+
+pub fn deploy_contract(name: &str, deployer: EvmAddress) -> EvmAddress {
+	deploy_contract_code(get_contract_bytecode(name), deployer)
+}
+
+pub fn deployer(who: AccountId) -> EvmAddress {
+	EVMAccounts::evm_address(&Into::<AccountId>::into(who))
+}
+
+pub fn deploy_token_contract(who: AccountId) -> EvmAddress {
+	deploy_contract("HydraToken", deployer(who))
+}
+
+fn bind_erc20(contract: EvmAddress) -> AssetId {
+	let token = CallContext::new_view(contract);
+	let asset = with_transaction(|| {
+		TransactionOutcome::Commit(AssetRegistry::register_sufficient_asset(
+			None,
+			Some(Erc20Currency::<Runtime>::name(token).unwrap().try_into().unwrap()),
+			AssetKind::Erc20,
+			1,
+			Some(Erc20Currency::<Runtime>::symbol(token).unwrap().try_into().unwrap()),
+			Some(Erc20Currency::<Runtime>::decimals(token).unwrap().try_into().unwrap()),
+			Some(AssetLocation(MultiLocation::new(
+				0,
+				X1(AccountKey20 {
+					key: contract.into(),
+					network: None,
+				}),
+			))),
+			None,
+		))
+	});
+	asset.unwrap()
+}
+
