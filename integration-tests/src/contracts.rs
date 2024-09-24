f51d5d91951b6ec21b0720679b4ffe5b155f7b68
mrq <mrq@mrq.cz>
2024-09-24 22:26:26 +0200

    final boss

--- integration-tests/src/contracts.rs
+++ integration-tests/src/contracts.rs
@@ -2,12 +2,19 @@ use crate::evm::dai_ethereum_address;
 use crate::polkadot_test_net::Hydra;
 use crate::polkadot_test_net::TestNet;
 use crate::polkadot_test_net::ALICE;
+use crate::polkadot_test_net::BOB;
+use crate::polkadot_test_net::UNITS;
+use crate::polkadot_test_net::WETH;
 use crate::utils::contracts::deploy_contract;
+use fp_evm::FeeCalculator;
+use frame_support::assert_ok;
 use hex_literal::hex;
 use hydradx_runtime::evm::Executor;
 use hydradx_runtime::AccountId;
 use hydradx_runtime::EVMAccounts;
 use hydradx_runtime::Runtime;
+use hydradx_runtime::RuntimeEvent;
+use hydradx_runtime::System;
 use hydradx_traits::evm::CallContext;
 use hydradx_traits::evm::EvmAddress;
 use hydradx_traits::evm::InspectEvmAccounts;
@@ -16,6 +23,7 @@ use num_enum::{IntoPrimitive, TryFromPrimitive};
 use pallet_evm::ExitReason::Succeed;
 use sp_core::H256;
 use sp_core::{RuntimeDebug, U256};
+use test_utils::expect_events;
 use xcm_emulator::Network;
 use xcm_emulator::TestExt;
 
@@ -77,12 +85,44 @@ fn contract_check_fails_on_precompile_without_code() {
 }
 
 #[test]
-fn contract_check_succeeds_on_currencies_precompile() {
+fn contract_check_should_succeed_when_called_from_extrinsic() {
 	TestNet::reset();
 	Hydra::execute_with(|| {
+		// Arrange
 		let checker = deploy_contract("ContractCheck", deployer());
+		let mut data = Into::<u32>::into(Function::Check).to_be_bytes().to_vec();
+		data.extend_from_slice(H256::from(dai_ethereum_address()).as_bytes());
+		assert_ok!(EVMAccounts::bind_evm_address(hydradx_runtime::RuntimeOrigin::signed(
+			BOB.into()
+		)));
+		assert_ok!(hydradx_runtime::Currencies::update_balance(
+			hydradx_runtime::RuntimeOrigin::root(),
+			BOB.into(),
+			WETH,
+			(10_000_000 * UNITS) as i128,
+		));
+		/// For reference with code set in storage this always succeeds
+		// pallet_evm::AccountCodes::<Runtime>::insert(
+		// 	dai_ethereum_address(),
+		// 	&hex!["365f5f375f5f365f73bebebebebebebebebebebebebebebebebebebebe5af43d5f5f3e5f3d91602a57fd5bf3"][..],
+		// );
 
-		assert_eq!(is_contract(checker, dai_ethereum_address()), true);
+		// Act
+		assert_ok!(hydradx_runtime::EVM::call(
+			hydradx_runtime::RuntimeOrigin::signed(BOB.into()),
+			EVMAccounts::evm_address(&Into::<AccountId>::into(BOB)),
+			checker,
+			data,
+			U256::from(0),
+			1000000,
+			hydradx_runtime::DynamicEvmFee::min_gas_price().0 * 10,
+			None,
+			Some(System::account_nonce(AccountId::from(BOB)).into()),
+			[].into()
+		));
+
+		// Assert
+		expect_events::<RuntimeEvent, Runtime>(vec![pallet_evm::Event::Executed { address: checker }.into()]);
 	});
 }
 
