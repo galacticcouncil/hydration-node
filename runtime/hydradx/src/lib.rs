b11e66a5ec955d3a5783596bf199a0afb56e5b93
mrq <mrq@mrq.cz>
2024-10-17 13:35:33 +0200

    versions bump

--- runtime/hydradx/src/lib.rs
+++ runtime/hydradx/src/lib.rs
@@ -112,7 +112,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("hydradx"),
 	impl_name: create_runtime_str!("hydradx"),
 	authoring_version: 1,
-	spec_version: 263,
+	spec_version: 264,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
