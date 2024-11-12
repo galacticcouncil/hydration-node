616c1ae4cf87a196fbae1cb510b31b0075d89980
Martin <martin.hloska@gmail.com>
2024-11-12 11:56:02 +0100

    disable submit for now

--- hydration-solver/src/lib.rs
+++ hydration-solver/src/lib.rs
@@ -9,7 +9,7 @@ use pallet_ice::api::ICEApi;
 use pallet_ice::traits::Solver;
 use pallet_ice::types::{BoundedResolvedIntents, BoundedTrades};
 use primitives::{AccountId, AssetId};
-use sc_client_api::{Backend, BlockchainEvents};
+use sc_client_api::{Backend, BlockchainEvents, UsageProvider};
 use sp_api::ProvideRuntimeApi;
 use sp_core::offchain::storage::OffchainDb;
 use sp_runtime::Permill;
@@ -24,8 +24,8 @@ pub struct HydrationSolver<T, RA, B, BE>(PhantomData<(T, RA, B, BE)>);
 impl<T, RA, Block, BE> HydrationSolver<T, RA, Block, BE>
 where
 	Block: sp_runtime::traits::Block,
-	RA: ProvideRuntimeApi<Block>,
-	RA::Api: pallet_ice::api::ICEApi<Block, AccountId, AssetId>,
+	RA: ProvideRuntimeApi<Block> + UsageProvider<Block>,
+	RA::Api: ICEApi<Block, AccountId, AssetId>,
 	BE: Backend<Block> + 'static,
 	RA: BlockchainEvents<Block> + 'static,
 	T: pallet_ice::Config
@@ -33,22 +33,29 @@ where
 		+ pallet_asset_registry::Config<AssetId = AssetId>
 		+ pallet_dynamic_fees::Config<Fee = Permill, AssetId = AssetId>,
 {
-	//pub async fn run<BE: BlockchainEvents<Block>>(client: Arc<BE>) {
 	pub async fn run(client: Arc<RA>) {
 		tracing::debug!(
 			target: LOG_TARGET,
 			"starting solver runner",
 		);
+
 		let mut notification_st = client.import_notification_stream();
 
 		while let Some(notification) = notification_st.next().await {
-			tracing::debug!(target: LOG_TARGET, "notification: {:?}", notification);
+			println!("notification: {:?}", notification);
 			let block_number: primitives::BlockNumber = 1; //TODO: get block number somehow
 			if notification.is_new_best {
-				tracing::debug!(target: LOG_TARGET, "is best");
+				//tracing::debug!(target: LOG_TARGET, "is best");
+				println!("is best");
+				let chain_info = client.usage_info().chain;
+				println!(
+					"chain info: {:?}",
+					chain_info.best_number,
+				);
 				let runtime = client.runtime_api();
 				let intents = runtime.intents(notification.hash, &notification.header).unwrap();
 
+				tracing::debug!(target: LOG_TARGET, "found {} intents", intents.len());
 				// Compute solution using solver
 				let Ok((resolved_intents, metadata)) =
 					omni::OmniSolver::<AccountId, AssetId, hydradx_adapters::ice::OmnipoolDataProvider<T>>::solve(
@@ -61,6 +68,7 @@ where
 
 				let (trades, score) = pallet_ice::Pallet::<T>::calculate_trades_and_score(&resolved_intents).unwrap();
 
+				/*
 				let call = pallet_ice::Call::propose_solution {
 					intents: BoundedResolvedIntents::truncate_from(resolved_intents),
 					trades: BoundedTrades::truncate_from(trades),
@@ -71,6 +79,8 @@ where
 					frame_system::offchain::SubmitTransaction::<T, pallet_ice::Call<T>>::submit_unsigned_transaction(
 						call.into(),
 					);
+
+				 */
 			}
 		}
 	}
