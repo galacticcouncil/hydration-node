2f1532c122fa81faf532a43348a3393ca649385e
Martin <martin.hloska@gmail.com>
2024-11-18 11:29:00 +0100

    fixes

--- hydration-solver/src/tests/v3.rs
+++ hydration-solver/src/tests/v3.rs
@@ -9,23 +9,45 @@ pub const ALICE: [u8; 32] = [4u8; 32];
 
 #[test]
 fn solver_should_find_solution_for_one_intent() {
-	let intents = vec![(
-		0,
-		Intent {
-			who: ALICE.into(),
-			swap: Swap {
-				asset_in: 0u32,
-				asset_out: 27u32,
-				amount_in: 100_000_000_000_000,
-				amount_out: 1_149_000_000_000,
-				swap_type: SwapType::ExactIn,
+	let intents = vec![
+		(
+			0,
+			Intent {
+				who: ALICE.into(),
+				swap: Swap {
+					asset_in: 0u32,
+					asset_out: 27u32,
+					amount_in: 100_000_000_000_000,
+					amount_out: 1_149_000_000_000,
+					swap_type: SwapType::ExactIn,
+				},
+				deadline: 0,
+				partial: true,
+				on_success: None,
+				on_failure: None,
 			},
-			deadline: 0,
-			partial: false,
-			on_success: None,
-			on_failure: None,
-		},
-	)];
+		),
+		/*
+		(
+			1,
+			Intent {
+				who: ALICE.into(),
+				swap: Swap {
+					asset_in: 5u32,
+					asset_out: 20u32,
+					amount_in: 100_000_000_000_000,
+					amount_out: 1_149_000_000_000,
+					swap_type: SwapType::ExactIn,
+				},
+				deadline: 0,
+				partial: true,
+				on_success: None,
+				on_failure: None,
+			},
+		)
+
+		 */
+	];
 	let (solution, _) = SolverV3::<DataProvider>::solve(intents).unwrap();
 	let expected_solution = vec![ResolvedIntent {
 		intent_id: 0,
