4aade8516e81b473e0daf3462454e644b9f85eac
Martin <martin.hloska@gmail.com>
2024-11-20 11:59:09 +0100

    fix indicators len

--- hydration-solver/src/tests/v3.rs
+++ hydration-solver/src/tests/v3.rs
@@ -8,7 +8,7 @@ use sp_core::crypto::AccountId32;
 pub const ALICE: [u8; 32] = [4u8; 32];
 
 #[test]
-fn solver_should_find_solution_for_one_intent() {
+fn solver_should_find_solution_for_one_small_amount_partial_intent() {
 	let intents = vec![
 		(
 			0,
@@ -27,32 +27,12 @@ fn solver_should_find_solution_for_one_intent() {
 				on_failure: None,
 			},
 		),
-		/*
-		(
-			1,
-			Intent {
-				who: ALICE.into(),
-				swap: Swap {
-					asset_in: 5u32,
-					asset_out: 20u32,
-					amount_in: 100_000_000_000_000,
-					amount_out: 1_149_000_000_000,
-					swap_type: SwapType::ExactIn,
-				},
-				deadline: 0,
-				partial: true,
-				on_success: None,
-				on_failure: None,
-			},
-		)
-
-		 */
 	];
 	let (solution, _) = SolverV3::<DataProvider>::solve(intents).unwrap();
 	let expected_solution = vec![ResolvedIntent {
 		intent_id: 0,
-		amount_in: 98465458599392,
-		amount_out: 1131368119307,
+		amount_in: 100_000_000_000_000,
+		amount_out: 1_149_000_000_000,
 	}];
 	assert_eq!(solution, expected_solution);
 }
