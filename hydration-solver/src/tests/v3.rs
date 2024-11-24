fb16a52fdcb926b026e4a88d91ffd6e30dd7b32f
Martin Hloska <martin.hloska@gmail.com>
2024-11-24 09:34:11 +0100

    add problematic 4 intent test

--- hydration-solver/src/tests/v3.rs
+++ hydration-solver/src/tests/v3.rs
@@ -210,8 +210,99 @@ fn solver_should_find_solution_for_two_partial_intents() {
 }
 
 #[test]
+fn solver_should_find_solution_for_four_intents() {
+	let intents = vec![
+		(
+			0,
+			Intent {
+				who: ALICE.into(),
+				swap: Swap {
+					asset_in: 13,
+					asset_out: 5,
+					amount_in: 514888002332937478066650,
+					amount_out: 664083505362373041510455118870258,
+					swap_type: SwapType::ExactIn,
+				},
+				deadline: 0,
+				partial: false,
+				on_success: None,
+				on_failure: None,
+			},
+		),
+		(
+			1,
+			Intent {
+				who: ALICE.into(),
+				swap: Swap {
+					asset_in: 20,
+					asset_out: 14,
+					amount_in: 165665617143487433531,
+					amount_out: 12177733280754553178994,
+					swap_type: SwapType::ExactIn,
+				},
+				deadline: 0,
+				partial: true,
+				on_success: None,
+				on_failure: None,
+			},
+		),
+		(
+			2,
+			Intent {
+				who: ALICE.into(),
+				swap: Swap {
+					asset_in: 0,
+					asset_out: 16,
+					amount_in: 25528234672916292207,
+					amount_out: 871403327041354,
+					swap_type: SwapType::ExactIn,
+				},
+				deadline: 0,
+				partial: false,
+				on_success: None,
+				on_failure: None,
+			},
+		),
+		(
+			3,
+			Intent {
+				who: ALICE.into(),
+				swap: Swap {
+					asset_in: 100,
+					asset_out: 101,
+					amount_in: 303603756622822659947591,
+					amount_out: 20555903343957624238452664953,
+					swap_type: SwapType::ExactIn,
+				},
+				deadline: 0,
+				partial: false,
+				on_success: None,
+				on_failure: None,
+			},
+		),
+	];
+	let start = Instant::now();
+	let (solution, _) = SolverV3::<DataProvider>::solve(intents).unwrap();
+	let duration = start.elapsed();
+	println!("Time elapsed in solve() is: {:?}", duration);
+	let expected_solution = vec![
+		ResolvedIntent {
+			intent_id: 0,
+			amount_in: 1_000_000_000_000_000_000,
+			amount_out: 1_149_000_000_000_000,
+		},
+		ResolvedIntent {
+			intent_id: 1,
+			amount_in: 36895351807444140032,
+			amount_out: 626344035537618048,
+		},
+	];
+	assert_eq!(solution, expected_solution);
+}
+
+#[test]
 fn solver_should_find_solution_for_many_intents() {
-	let intents = generate_random_intents(10, DataProvider::assets(None));
+	let intents = generate_random_intents(4, DataProvider::assets(None));
 	dbg!(&intents);
 	let start = Instant::now();
 	let (solution, _) = SolverV3::<DataProvider>::solve(intents).unwrap();
