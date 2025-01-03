94be437e3e19b2bc6217d7997ddcc0749c42588a
dmoka <mokabme@gmail.com>
2025-01-03 16:48:05 +0100

    bump runtime version

--- runtime/hydradx/src/lib.rs
+++ runtime/hydradx/src/lib.rs
@@ -113,7 +113,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("hydradx"),
 	impl_name: create_runtime_str!("hydradx"),
 	authoring_version: 1,
-	spec_version: 277,
+	spec_version: 278,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
