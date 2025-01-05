4e194f9a588b5484016a3a46a788390dcfa4f938
Martin Hloska <martin.hloska@gmail.com>
2025-01-05 15:53:27 +0100

    remove old dynamic fee calculation

--- math/src/dynamic_fees/math.rs
+++ math/src/dynamic_fees/math.rs
@@ -13,15 +13,23 @@ use sp_arithmetic::{FixedPointOperand, FixedU128, PerThing};
 /// `last_block_diff` is the difference between the current block height and the previous block height when asset fee was calculated.
 /// `params` is the fee parameters, such as minimum fee, maximum fee, decay and amplification.
 pub fn recalculate_asset_fee<Fee: PerThing>(
-	volume: OracleEntry,
+	last_entry: OracleEntry,
+	current_asset_liquidity: u128,
 	previous_fee: Fee,
-	last_block_diff: u128,
+	block_diff: u128,
 	params: FeeParams<Fee>,
 ) -> Fee
 where
 	<Fee as PerThing>::Inner: FixedPointOperand,
 {
-	recalculate_fee(volume, previous_fee, last_block_diff, params, OutIn)
+	compute_dynamic_fee(
+		last_entry,
+		current_asset_liquidity,
+		params,
+		previous_fee,
+		block_diff,
+		OutIn,
+	)
 }
 
 /// Recalculate Omnipool's protocol fee given previously calculated fee and oracle data.
@@ -31,69 +39,25 @@ where
 /// `last_block_diff` is the difference between the current block height and the previous block height when asset fee was calculated.
 /// `params` is the fee parameters, such as minimum fee, maximum fee, decay and amplification.
 pub fn recalculate_protocol_fee<Fee: PerThing>(
-	volume: OracleEntry,
+	last_entry: OracleEntry,
+	current_asset_liquidity: u128,
 	previous_fee: Fee,
-	last_block_diff: u128,
+	block_diff: u128,
 	params: FeeParams<Fee>,
 ) -> Fee
 where
 	<Fee as PerThing>::Inner: FixedPointOperand,
 {
-	recalculate_fee(volume, previous_fee, last_block_diff, params, InOut)
+	compute_dynamic_fee(
+		last_entry,
+		current_asset_liquidity,
+		params,
+		previous_fee,
+		block_diff,
+		InOut,
+	)
 }
-
-fn recalculate_fee<Fee: PerThing>(
-	volume: OracleEntry,
-	previous_fee: Fee,
-	last_block_diff: u128,
-	params: FeeParams<Fee>,
-	direction: NetVolumeDirection,
-) -> Fee
-where
-	<Fee as PerThing>::Inner: FixedPointOperand,
-{
-	// Adjust previous fee which may not have been calculated in previous block
-	let fixed_previous_fee = if last_block_diff > 1 {
-		let decaying = params
-			.decay
-			.saturating_mul(FixedU128::from(last_block_diff.saturating_sub(1)));
-		FixedU128::from(previous_fee)
-			.saturating_sub(decaying)
-			.max(params.min_fee.into())
-	} else {
-		previous_fee.into()
-	};
-
-	// 1. if asset fee, direction OutIn : x = (Vo - Vi) / L
-	// 2. if protocol fee, direction InOut: x = (Vi - Vo) / L
-	let (x, x_neg) = if !volume.liquidity.is_zero() {
-		let (diff, neg) = volume.net_volume(direction);
-		(FixedU128::from_rational(diff, volume.liquidity), neg)
-	} else {
-		(FixedU128::zero(), false)
-	};
-
-	let a_x = params.amplification.saturating_mul(x);
-
-	// Work out fee adjustment taking into account possible negative result
-	let (delta_f, neg) = if x_neg {
-		(a_x.saturating_add(params.decay), true)
-	} else if a_x > params.decay {
-		(a_x.saturating_sub(params.decay), false)
-	} else {
-		(params.decay.saturating_sub(a_x), true)
-	};
-
-	if neg {
-		fixed_previous_fee.saturating_sub(delta_f)
-	} else {
-		fixed_previous_fee.saturating_add(delta_f)
-	}
-	.into_clamped_perthing::<Fee>()
-	.clamp(params.min_fee, params.max_fee)
-}
-
-pub(crate) fn compute_dynamic_fee<Fee: PerThing>(
+pub(super) fn compute_dynamic_fee<Fee: PerThing>(
 	last_entry: OracleEntry,
 	liquidity: u128,
 	params: FeeParams<Fee>,
@@ -107,10 +71,8 @@ where
 	if params.amplification.is_zero() || block_diff.is_zero() {
 		return previous_fee;
 	}
-
-	// TODO: decay 0 - return previous fee?!! otherwise division by zero
-	if params.decay.is_zero() {
-		return previous_fee;
+	if last_entry.decay_factor.is_zero() {
+		return previous_fee; //otherwise we would divide by zero
 	}
 
 	let (net_volume, neg) = last_entry.net_volume(net_direction);
--- math/src/dynamic_fees/tests.rs
+++ math/src/dynamic_fees/tests.rs
@@ -3,14 +3,13 @@ use crate::dynamic_fees::{compute_dynamic_fee, recalculate_asset_fee, recalculat
 use num_traits::Zero;
 use sp_arithmetic::{FixedU128, Permill};
 
-/*
 #[test]
 fn asset_fee_should_decrease_when_in_is_greater_than_out() {
-	// Test parameters
 	let volume = OracleEntry {
 		amount_in: 25,
 		amount_out: 20,
 		liquidity: 1000,
+		decay_factor: FixedU128::from_rational(2, 10),
 	};
 	let previous_fee = Permill::from_percent(10);
 	let last_block_diff = 1;
@@ -23,17 +22,17 @@ fn asset_fee_should_decrease_when_in_is_greater_than_out() {
 
 	let expected_fee = Permill::from_percent(9);
 
-	let calculated_fee = recalculate_asset_fee(volume, previous_fee, last_block_diff, params);
+	let calculated_fee = recalculate_asset_fee(volume, 1025, previous_fee, last_block_diff, params);
 	assert_eq!(calculated_fee, expected_fee);
 }
 
 #[test]
 fn asset_fee_should_increase_when_out_is_greater_than_int() {
-	// Test parameters
 	let volume = OracleEntry {
 		amount_in: 5,
 		amount_out: 20,
 		liquidity: 1000,
+		decay_factor: FixedU128::from_rational(2, 10),
 	};
 	let previous_fee = Permill::from_percent(10);
 	let last_block_diff = 1;
@@ -46,17 +45,17 @@ fn asset_fee_should_increase_when_out_is_greater_than_int() {
 
 	let expected_fee = Permill::from_percent(13);
 
-	let calculated_fee = recalculate_asset_fee(volume, previous_fee, last_block_diff, params);
+	let calculated_fee = recalculate_asset_fee(volume, 980, previous_fee, last_block_diff, params);
 	assert_eq!(calculated_fee, expected_fee);
 }
 
 #[test]
 fn protocol_fee_should_decrease_when_out_is_greater_than_int() {
-	// Test parameters
 	let volume = OracleEntry {
 		amount_in: 5,
 		amount_out: 20,
 		liquidity: 1000,
+		decay_factor: FixedU128::from_rational(2, 10),
 	};
 	let previous_fee = Permill::from_percent(10);
 	let last_block_diff = 1;
@@ -69,17 +68,17 @@ fn protocol_fee_should_decrease_when_out_is_greater_than_int() {
 
 	let expected_fee = Permill::from_percent(7);
 
-	let calculated_fee = recalculate_protocol_fee(volume, previous_fee, last_block_diff, params);
+	let calculated_fee = recalculate_protocol_fee(volume, 985, previous_fee, last_block_diff, params);
 	assert_eq!(calculated_fee, expected_fee);
 }
 
 #[test]
 fn protocol_fee_should_increase_when_in_is_greater_than_out() {
-	// Test parameters
 	let volume = OracleEntry {
 		amount_in: 25,
 		amount_out: 20,
 		liquidity: 1000,
+		decay_factor: FixedU128::from_rational(2, 10),
 	};
 	let previous_fee = Permill::from_percent(10);
 	let last_block_diff = 1;
@@ -90,65 +89,19 @@ fn protocol_fee_should_increase_when_in_is_greater_than_out() {
 		max_fee: Permill::from_percent(30),
 	};
 
-	let expected_fee = Permill::from_percent(11);
+	let expected_fee = Permill::from_float(0.109999);
 
-	let calculated_fee = recalculate_protocol_fee(volume, previous_fee, last_block_diff, params);
-	assert_eq!(calculated_fee, expected_fee);
-}
-
-#[test]
-fn protocol_fee_should_decay_when_block_diff_is_greater_than_one() {
-	// Test parameters
-	let volume = OracleEntry {
-		amount_in: 25,
-		amount_out: 20,
-		liquidity: 1000,
-	};
-	let previous_fee = Permill::from_percent(10);
-	let last_block_diff = 3;
-	let params = FeeParams {
-		amplification: FixedU128::from(2),
-		decay: FixedU128::from_float(0.001),
-		min_fee: Permill::from_percent(1),
-		max_fee: Permill::from_percent(30),
-	};
-
-	let expected_fee = Permill::from_float(0.107);
-
-	let calculated_fee = recalculate_protocol_fee(volume, previous_fee, last_block_diff, params);
-	assert_eq!(calculated_fee, expected_fee);
-}
-
-#[test]
-fn asset_fee_should_decay_when_block_diff_is_greater_than_one() {
-	// Test parameters
-	let volume = OracleEntry {
-		amount_in: 25,
-		amount_out: 20,
-		liquidity: 1000,
-	};
-	let previous_fee = Permill::from_percent(10);
-	let last_block_diff = 3;
-	let params = FeeParams {
-		amplification: FixedU128::from(2),
-		decay: FixedU128::from_float(0.001),
-		min_fee: Permill::from_percent(1),
-		max_fee: Permill::from_percent(30),
-	};
-
-	let expected_fee = Permill::from_float(0.087);
-
-	let calculated_fee = recalculate_asset_fee(volume, previous_fee, last_block_diff, params);
+	let calculated_fee = recalculate_protocol_fee(volume, 1005, previous_fee, last_block_diff, params);
 	assert_eq!(calculated_fee, expected_fee);
 }
 
 #[test]
 fn asset_fee_should_clamp_to_max_fee() {
-	// Test parameters
 	let volume = OracleEntry {
 		amount_in: 5,
 		amount_out: 20,
 		liquidity: 100,
+		decay_factor: FixedU128::from_rational(2, 10),
 	};
 	let previous_fee = Permill::from_percent(10);
 	let last_block_diff = 3;
@@ -161,17 +114,17 @@ fn asset_fee_should_clamp_to_max_fee() {
 
 	let expected_fee = Permill::from_percent(30);
 
-	let calculated_fee = recalculate_asset_fee(volume, previous_fee, last_block_diff, params);
+	let calculated_fee = recalculate_asset_fee(volume, 85, previous_fee, last_block_diff, params);
 	assert_eq!(calculated_fee, expected_fee);
 }
 
 #[test]
 fn asset_fee_should_clamp_to_min_fee() {
-	// Test parameters
 	let volume = OracleEntry {
 		amount_in: 25,
 		amount_out: 20,
 		liquidity: 100,
+		decay_factor: FixedU128::from_rational(2, 10),
 	};
 	let previous_fee = Permill::from_percent(10);
 	let last_block_diff = 3;
@@ -184,17 +137,17 @@ fn asset_fee_should_clamp_to_min_fee() {
 
 	let expected_fee = Permill::from_percent(1);
 
-	let calculated_fee = recalculate_asset_fee(volume, previous_fee, last_block_diff, params);
+	let calculated_fee = recalculate_asset_fee(volume, 105, previous_fee, last_block_diff, params);
 	assert_eq!(calculated_fee, expected_fee);
 }
 
 #[test]
 fn procotol_fee_should_clamp_to_min_fee() {
-	// Test parameters
 	let volume = OracleEntry {
 		amount_in: 5,
 		amount_out: 20,
 		liquidity: 100,
+		decay_factor: FixedU128::from_rational(2, 10),
 	};
 	let previous_fee = Permill::from_percent(10);
 	let last_block_diff = 3;
@@ -207,17 +160,17 @@ fn procotol_fee_should_clamp_to_min_fee() {
 
 	let expected_fee = Permill::from_percent(1);
 
-	let calculated_fee = recalculate_protocol_fee(volume, previous_fee, last_block_diff, params);
+	let calculated_fee = recalculate_protocol_fee(volume, 85, previous_fee, last_block_diff, params);
 	assert_eq!(calculated_fee, expected_fee);
 }
 
 #[test]
 fn protocol_fee_should_clamp_to_max_fee() {
-	// Test parameters
 	let volume = OracleEntry {
 		amount_in: 25,
 		amount_out: 20,
 		liquidity: 100,
+		decay_factor: FixedU128::from_rational(2, 10),
 	};
 	let previous_fee = Permill::from_percent(10);
 	let last_block_diff = 3;
@@ -230,11 +183,9 @@ fn protocol_fee_should_clamp_to_max_fee() {
 
 	let expected_fee = Permill::from_percent(15);
 
-	let calculated_fee = recalculate_protocol_fee(volume, previous_fee, last_block_diff, params);
+	let calculated_fee = recalculate_protocol_fee(volume, 105, previous_fee, last_block_diff, params);
 	assert_eq!(calculated_fee, expected_fee);
 }
-
- */
 
 #[test]
 fn compute_asset_fee_should_increase_when_in_is_greater_than_out() {
