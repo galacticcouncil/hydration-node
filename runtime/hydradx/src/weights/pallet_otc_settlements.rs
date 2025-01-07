49cf212d3c7fff4a917f08129c1e1b8a448f19e9
dmoka <mokabme@gmail.com>
2025-01-07 12:12:52 +0100

    fix otc settlement weight

--- runtime/hydradx/src/weights/pallet_otc_settlements.rs
+++ runtime/hydradx/src/weights/pallet_otc_settlements.rs
@@ -52,11 +52,8 @@ use frame_support::{traits::Get, weights::{Weight, constants::RocksDbWeight}};
 use core::marker::PhantomData;
 
 /// Weights for `pallet_otc_settlements`.
-pub struct WeightInfo<T>(PhantomData<T>);
-
-/// Weights for `pallet_otc_settlements` using the HydraDX node and recommended hardware.
 pub struct HydraWeight<T>(PhantomData<T>);
-impl<T: frame_system::Config> pallet_otc_settlements::WeightInfo for HydraWeight<T> {
+impl<T: frame_system::Config> pallet_otc_settlements::weights::WeightInfo for HydraWeight<T> {
 	/// Storage: `OTC::Orders` (r:1 w:0)
 	/// Proof: `OTC::Orders` (`max_values`: None, `max_size`: Some(93), added: 2568, mode: `MaxEncodedLen`)
 	/// Storage: `System::Account` (r:2 w:1)
