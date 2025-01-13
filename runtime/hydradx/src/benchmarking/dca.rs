81294afd4eac5b8fad5aaa0b820ea3d4c5171353
dmoka <mokabme@gmail.com>
2025-01-13 15:01:11 +0100

    make terminate benchmark more worst case

--- runtime/hydradx/src/benchmarking/dca.rs
+++ runtime/hydradx/src/benchmarking/dca.rs
@@ -489,7 +489,7 @@ runtime_benchmarks! {
 		let execution_block = 100u32;
 		assert_ok!(DCA::schedule(RawOrigin::Signed(caller).into(), schedule1, Option::Some(execution_block)));
 
-	}: _(RawOrigin::Root, schedule_id, Some(105))
+	}: _(RawOrigin::Root, schedule_id, None)
 	verify {
 		assert!(<Schedules<Runtime>>::get::<ScheduleId>(schedule_id).is_none());
 	}
