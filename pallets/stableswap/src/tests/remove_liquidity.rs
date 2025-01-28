f04693829cee1f2d860a89f63f4ca577ed2e6a45
dmoka <mokabme@gmail.com>
2025-01-28 14:00:58 +0100

    wip - add test for removing check

--- pallets/stableswap/src/tests/remove_liquidity.rs
+++ pallets/stableswap/src/tests/remove_liquidity.rs
@@ -851,6 +851,76 @@ fn removing_liquidity_with_exact_amount_should_work() {
 }
 
 #[test]
+fn removing_liquidity_with_exact_amount_should_work_when_dust_left() {
+	let asset_a: AssetId = 1;
+	let asset_b: AssetId = 2;
+	let asset_c: AssetId = 3;
+
+	ExtBuilder::default()
+		.with_endowed_accounts(vec![
+			(BOB, asset_a, 2_000_000_000_000_000_003),
+			(ALICE, asset_a, 52425995641788588073263117),
+			(ALICE, asset_b, 52033213790329),
+			(ALICE, asset_c, 119135337044269),
+		])
+		.with_registered_asset("one".as_bytes().to_vec(), asset_a, 18)
+		.with_registered_asset("two".as_bytes().to_vec(), asset_b, 6)
+		.with_registered_asset("three".as_bytes().to_vec(), asset_c, 6)
+		.with_pool(
+			ALICE,
+			PoolInfo::<AssetId, u64> {
+				assets: vec![asset_a, asset_b, asset_c].try_into().unwrap(),
+				initial_amplification: NonZeroU16::new(2000).unwrap(),
+				final_amplification: NonZeroU16::new(2000).unwrap(),
+				initial_block: 0,
+				final_block: 0,
+				fee: Permill::zero(),
+			},
+			InitialLiquidity {
+				account: ALICE,
+				assets: vec![
+					AssetAmount::new(asset_a, 52425995641788588073263117),
+					AssetAmount::new(asset_b, 52033213790329),
+					AssetAmount::new(asset_c, 119135337044269),
+				],
+			},
+		)
+		.build()
+		.execute_with(|| {
+			let pool_id = get_pool_id_at(0);
+			let amount = 2_000_000_000_000_000_000;
+			Tokens::withdraw(pool_id, &ALICE, 5906657405945079804575283).unwrap();
+			let desired_shares = 1947597621401945851;
+			assert_ok!(Stableswap::add_liquidity_shares(
+				RuntimeOrigin::signed(BOB),
+				pool_id,
+				desired_shares,
+				asset_a,
+				amount + 3, // add liquidity for shares uses slightly more
+			));
+			let received = Tokens::free_balance(pool_id, &BOB);
+			assert_eq!(received, desired_shares);
+			let balance = Tokens::free_balance(asset_a, &BOB);
+			assert_eq!(balance, 0);
+			// ACT
+			assert_ok!(Stableswap::withdraw_asset_amount(
+				RuntimeOrigin::signed(BOB),
+				pool_id,
+				asset_a,
+				amount - 1, maybe here remove more or so
+				desired_shares,
+			));
+
+			// ASSERT
+
+			let received = Tokens::free_balance(pool_id, &BOB);
+			assert_eq!(received, 0);
+			let balance = Tokens::free_balance(asset_a, &BOB);
+			assert_eq!(balance, 1_999_999_999_999_999_999);
+		});
+}
+
+#[test]
 fn removing_liquidity_should_not_give_more_assets() {
 	let asset_a: AssetId = 1;
 	let asset_b: AssetId = 2;
