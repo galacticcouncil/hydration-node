03179518abdfe17c8323b7ee950e745f60c2b338
vgantchev <v@lery.dev>
2025-02-06 17:06:57 +0100

    lower fees

--- runtime/hydradx/src/assets.rs
+++ runtime/hydradx/src/assets.rs
@@ -1269,7 +1269,7 @@ parameter_types! {
 	pub ProtocolFeeParams: FeeParams<Permill> = FeeParams{
 		min_fee: Permill::from_rational(25u32,10000u32), // 0.25%
 		max_fee: Permill::from_rational(1u32,1000u32),   // 0.1%
-		decay: FixedU128::from_rational(5,10000),        // 0.05%
+		decay: FixedU128::from_rational(5,100000),       // 0.005%
 		amplification: FixedU128::one(),                 // 1
 	};
 
