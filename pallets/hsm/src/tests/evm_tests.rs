6199a7b3080ae26dc39264c5420c82b0021fb602
Martin Hloska <martin.hloska@gmail.com>
2025-04-09 12:10:54 +0200

    fix evm tests

--- pallets/hsm/src/tests/evm_tests.rs
+++ pallets/hsm/src/tests/evm_tests.rs
@@ -32,8 +32,10 @@ fn test_mint_hollar_evm() {
 		let amount = 100 * ONE;
 		assert_ok!(HSM::mint_hollar(&ALICE, amount));
 
+		let alice_evm_addr = <Test as pallet::Config>::EvmAccounts::evm_address(&ALICE);
+		let alice_evm = MockEvmAccounts::account_id(alice_evm_addr);
 		// Check if the balance was updated
-		assert_balance!(ALICE, HOLLAR, amount);
+		assert_balance!(alice_evm, HOLLAR, amount);
 
 		// Check if the EVM call was made with the right data
 		let evm_calls = last_evm_call();
@@ -41,30 +43,6 @@ fn test_mint_hollar_evm() {
 
 		// Clear calls for next test
 		clear_evm_calls();
-	});
-}
-
-#[test]
-fn test_burn_hollar_evm() {
-	let mut ext = ExtBuilder::default()
-		.with_endowed_accounts(vec![(ALICE, HDX, 10_000 * ONE), (ALICE, HOLLAR, 500 * ONE)])
-		.with_registered_asset(HOLLAR, 18)
-		.build();
-
-	ext.execute_with(|| {
-		let initial_balance = Tokens::free_balance(HOLLAR, &ALICE);
-		assert_eq!(initial_balance, 500 * ONE);
-
-		let burn_amount = 100 * ONE;
-		assert_ok!(HSM::burn_hollar(burn_amount));
-
-		// Check if the balance was updated correctly
-		let expected_balance = initial_balance - burn_amount;
-		assert_balance!(ALICE, HOLLAR, expected_balance);
-
-		// Check if the EVM call was made
-		let evm_calls = last_evm_call();
-		assert!(evm_calls.is_some());
 	});
 }
 
@@ -79,14 +57,29 @@ fn test_mint_and_burn_flow() {
 		// First mint some Hollar
 		let mint_amount = 200 * ONE;
 		assert_ok!(HSM::mint_hollar(&ALICE, mint_amount));
-		assert_balance!(ALICE, HOLLAR, mint_amount);
+
+		let alice_evm_addr = <Test as pallet::Config>::EvmAccounts::evm_address(&ALICE);
+		let alice_evm = MockEvmAccounts::account_id(alice_evm_addr);
+		assert_balance!(alice_evm, HOLLAR, mint_amount);
+
+		let burn_amount = 100 * ONE;
+
+		let pallet_evm_addr = <Test as pallet::Config>::EvmAccounts::evm_address(&HSM::account_id());
+		let pallet_evm = MockEvmAccounts::account_id(pallet_evm_addr);
+
+		//transfer to pallet account
+		assert_ok!(Tokens::transfer(
+			RuntimeOrigin::signed(alice_evm),
+			pallet_evm.clone(),
+			HOLLAR,
+			burn_amount
+		));
+		assert_balance!(pallet_evm, HOLLAR, burn_amount);
 
 		// Then burn half of it
-		let burn_amount = 100 * ONE;
 		assert_ok!(HSM::burn_hollar(burn_amount));
 
 		// Verify the balance is as expected
-		let expected_balance = mint_amount - burn_amount;
-		assert_balance!(ALICE, HOLLAR, expected_balance);
+		assert_balance!(pallet_evm, HOLLAR, 0);
 	});
 }
