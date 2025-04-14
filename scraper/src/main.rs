f243521152697e0947dcef548273bbc60ab61215
dmoka <mokabme@gmail.com>
2025-04-14 13:30:57 +0200

    Revert "change scraper export state to work based on hashes"
    This reverts commit 0dbf7d2622dcb128cfae89aebc6a3ca009ad5273.

--- scraper/src/main.rs
+++ scraper/src/main.rs
@@ -47,26 +47,12 @@ struct ExportStateCmd {
 	shared: SharedParams,
 }
 
-#[derive(Parser, Debug)]
-struct CreateChainspecCmd {
-	/// The path to the SNAPSHOT file
-	#[arg(long)]
-	snapshot: PathBuf,
-	/// The path where to save the chainspec file
-	#[arg(long)]
-	output: PathBuf,
-	#[allow(missing_docs)]
-	#[clap(flatten)]
-	shared: SharedParams,
-}
-
 /// Possible commands of `scraper`.
 #[derive(Parser, Debug)]
 enum Command {
 	SaveStorage(StorageCmd),
 	SaveBlocks(BlocksCmd),
 	ExportState(ExportStateCmd),
-	CreateChainspec(CreateChainspecCmd),
 }
 
 /// Shared parameters of the `scraper` commands.
@@ -204,25 +190,6 @@ fn main() {
 					scraper::save_chainspec(builder, path.clone(), cmd.shared.uri)
 						.await
 						.unwrap()
-				});
-
-			path
-		}
-		Command::CreateChainspec(cmd) => {
-			let path = cmd.output.clone();
-
-			tokio::runtime::Builder::new_current_thread()
-				.enable_all()
-				.build()
-				.unwrap()
-				.block_on(async {
-					scraper::create_chainspec_from_snapshot::<hydradx_runtime::Block>(
-						cmd.snapshot,
-						path.clone(),
-						cmd.shared.uri,
-					)
-					.await
-					.unwrap()
 				});
 
 			path
