a84d96fd3cc2d8b1a9ef98da161b029e01aa12b9
Vlad Proshchavaiev <32250097+F3Joule@users.noreply.github.com>
2025-06-06 08:38:17 +0300

    Add hyperbridge to a runtime

--- runtime/hydradx/src/lib.rs
+++ runtime/hydradx/src/lib.rs
@@ -248,6 +248,11 @@ construct_runtime!(
 		Aura: pallet_aura = 167,
 		AuraExt: cumulus_pallet_aura_ext = 169,
 
+		// Hyperbridge
+		Ismp: pallet_ismp = 180,
+		IsmpParachain: ismp_parachain = 181,
+		Hyperbridge: pallet_hyperbridge = 182,
+
 		// Warehouse - let's allocate indices 100+ for warehouse pallets
 		EmaOracle: pallet_ema_oracle = 202,
 		Broadcast: pallet_broadcast = 204,
@@ -348,6 +353,7 @@ mod benches {
 		[pallet_whitelist, Whitelist]
 		[pallet_dispatcher, Dispatcher]
 		[pallet_hsm, HSM]
+		[ismp_parachain, IsmpParachain]
 	);
 }
 
@@ -456,6 +462,7 @@ impl fp_rpc::ConvertTransaction<sp_runtime::OpaqueExtrinsic> for TransactionConv
 use crate::evm::aave_trade_executor::AaveTradeExecutor;
 use crate::evm::aave_trade_executor::PoolData;
 use crate::evm::precompiles::erc20_mapping::HydraErc20Mapping;
+use cumulus_pallet_parachain_system::RelayChainState;
 use frame_support::{
 	genesis_builder_helper::{build_state, get_preset},
 	sp_runtime::{
@@ -465,6 +472,10 @@ use frame_support::{
 	weights::WeightToFee as _,
 };
 use hydradx_traits::evm::Erc20Mapping;
+use ismp::{
+	consensus::{ConsensusClientId, StateMachineHeight, StateMachineId},
+	host::StateMachine,
+};
 use pallet_liquidation::BorrowingContract;
 use pallet_route_executor::TradeExecution;
 pub use polkadot_xcm::latest::Junction;
@@ -1117,6 +1128,62 @@ impl_runtime_apis! {
 
 		fn pools() -> Vec<PoolData<Balance>> {
 			Self::pairs().into_iter().map(|p| Self::pool(p.0, p.1)).collect()
+		}
+	}
+
+	// Hyperbridge
+	impl pallet_ismp_runtime_api::IsmpRuntimeApi<Block, <Block as BlockT>::Hash> for Runtime {
+		fn host_state_machine() -> StateMachine {
+			<Runtime as pallet_ismp::Config>::HostStateMachine::get()
+		}
+
+		fn challenge_period(state_machine_id: StateMachineId) -> Option<u64> {
+			pallet_ismp::Pallet::<Runtime>::challenge_period(state_machine_id)
+		}
+
+		/// Fetch all ISMP events in the block, should only be called from runtime-api.
+		fn block_events() -> Vec<::ismp::events::Event> {
+			pallet_ismp::Pallet::<Runtime>::block_events()
+		}
+
+		/// Fetch all ISMP events and their extrinsic metadata, should only be called from runtime-api.
+		fn block_events_with_metadata() -> Vec<(::ismp::events::Event, Option<u32>)> {
+			pallet_ismp::Pallet::<Runtime>::block_events_with_metadata()
+		}
+
+		/// Return the scale encoded consensus state
+		fn consensus_state(id: ConsensusClientId) -> Option<Vec<u8>> {
+			pallet_ismp::Pallet::<Runtime>::consensus_states(id)
+		}
+
+		/// Return the timestamp this client was last updated in seconds
+		fn state_machine_update_time(height: StateMachineHeight) -> Option<u64> {
+			pallet_ismp::Pallet::<Runtime>::state_machine_update_time(height)
+		}
+
+		/// Return the latest height of the state machine
+		fn latest_state_machine_height(id: StateMachineId) -> Option<u64> {
+			pallet_ismp::Pallet::<Runtime>::latest_state_machine_height(id)
+		}
+
+		/// Get actual requests
+		fn requests(commitments: Vec<H256>) -> Vec<ismp::router::Request> {
+			pallet_ismp::Pallet::<Runtime>::requests(commitments)
+		}
+
+		/// Get actual requests
+		fn responses(commitments: Vec<H256>) -> Vec<ismp::router::Response> {
+			pallet_ismp::Pallet::<Runtime>::responses(commitments)
+		}
+	}
+
+	impl ismp_parachain_runtime_api::IsmpParachainApi<Block> for Runtime {
+		fn para_ids() -> Vec<u32> {
+			IsmpParachain::para_ids()
+		}
+
+		fn current_relay_chain_state() -> RelayChainState {
+			IsmpParachain::current_relay_chain_state()
 		}
 	}
 
