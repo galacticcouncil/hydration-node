26e808f9db2685b2625ee431b36b686540cc4528
cl0w <v@lery.dev>
2026-01-15 16:01:35 +0100

    versions

--- runtime/hydradx/src/lib.rs
+++ runtime/hydradx/src/lib.rs
@@ -129,7 +129,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: Cow::Borrowed("hydradx"),
 	impl_name: Cow::Borrowed("hydradx"),
 	authoring_version: 1,
-	spec_version: 382,
+	spec_version: 383,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
