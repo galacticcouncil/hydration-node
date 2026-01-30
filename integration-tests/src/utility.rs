52b53e97e42a391fc8cb8867261fe42c0efd9ab0
dmoka <mokabme@gmail.com>
2026-01-30 14:43:56 +0100

    formatting

--- integration-tests/src/utility.rs
+++ integration-tests/src/utility.rs
@@ -5,10 +5,10 @@ use frame_support::assert_ok;
 use pallet_broadcast::types::Destination;
 
 use crate::assert_balance;
+use hydradx_runtime::LBP;
 use hydradx_runtime::XYK;
 use hydradx_runtime::{Currencies, Omnipool, Runtime};
 use hydradx_runtime::{RuntimeCall, Utility};
-use hydradx_runtime::{LBP};
 use hydradx_traits::router::PoolType;
 use pallet_broadcast::types::Asset;
 use xcm_emulator::TestExt;
