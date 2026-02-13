ebcaf744600e9eba6f011ec9411ad97feea88052
vgantchev <v@lery.dev>
2026-02-13 14:25:05 +0100

    upgrade benchmarks to stable2506

--- runtime/hydradx/src/system.rs
+++ runtime/hydradx/src/system.rs
@@ -482,6 +482,7 @@ impl pallet_identity::Config for Runtime {
 	type WeightInfo = weights::pallet_identity::HydraWeight<Runtime>;
 	type UsernameDeposit = UsernameDeposit;
 	type UsernameGracePeriod = ConstU32<{ 30 * DAYS }>;
+	#[cfg(feature = "runtime-benchmarks")]
 	type BenchmarkHelper = ();
 }
 
