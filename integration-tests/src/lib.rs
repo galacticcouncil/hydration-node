a553d9fc9972ab137891a2827b5f82b1265a2590
Martin Hloska <martin.hloska@gmail.com>
2026-02-23 09:51:30 +0100

    slip fees eval tests

--- integration-tests/src/lib.rs
+++ integration-tests/src/lib.rs
@@ -33,6 +33,7 @@ mod omnipool_init;
 mod omnipool_liquidity_mining;
 mod omnipool_remove_all_liquidity;
 mod omnipool_slip_fees;
+mod omnipool_slip_fees_xval;
 mod oracle;
 mod otc;
 mod parameters;
