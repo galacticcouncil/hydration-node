2da86c229a6f0e6a4fa421996a74e9ce9b98a525
Martin Hloska <martin.hloska@gmail.com>
2026-05-10 13:23:41 +0200

    fix tests due to weight changes

--- integration-tests/src/exchange_asset.rs
+++ integration-tests/src/exchange_asset.rs
@@ -999,7 +999,7 @@ mod circuit_breaker {
 		Hydra::execute_with(|| {
 			let trapped_event = last_hydra_events(10)[3].clone(); //We need to explicitly assert it, so we can be flexible with amount assertion. If it changes, debug and see at which index is the PolkadotXcm TrappedAsset event
 
-			assert_trapped_acala_token(&trapped_event, 490054588142157u128);
+			assert_trapped_acala_token(&trapped_event, 489124324725094u128);
 
 			//Assert that nothing was reserved on TempAccountForXcmAssetExchange
 			assert_reserved_balance!(TempAccountForXcmAssetExchange::get(), ACA, 0u128);
@@ -1207,7 +1207,7 @@ mod circuit_breaker {
 			let trapped_event = &last_hydra_events(10)[3].clone();
 			let trapped_amount = get_trapped_amount(trapped_event);
 
-			assert_trapped_acala_token(trapped_event, 89893499361959u128);
+			assert_trapped_acala_token(trapped_event, 89124324725094u128);
 
 			let fee = hydradx_runtime::Tokens::free_balance(ACA, &hydradx_runtime::Treasury::account_id());
 
