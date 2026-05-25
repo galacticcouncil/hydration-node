97436f3dd7d51884c865452bdc5c59737dedc47e
vgantchev <v@lery.dev>
2026-05-25 09:35:28 +0200

    bounded conviction voting migration

--- runtime/hydradx/src/migrations/conviction_voting.rs
+++ runtime/hydradx/src/migrations/conviction_voting.rs
@@ -9,6 +9,7 @@ use sp_runtime::traits::{BlockNumberProvider, Saturating};
 use sp_std::marker::PhantomData;
 
 const MIGRATION_DONE_KEY: &[u8] = b"HydrationConvictionVoting2sBlockMigrationDone";
+const MAX_VOTING_FOR_RECORDS: u64 = 1_000;
 
 // This migration preserves existing conviction-voting prior-lock wall-clock unlock times when
 // moving from 6s to 2s blocks. Only `VotingFor` stores block numbers; `ClassLocksFor` stores
@@ -16,9 +17,9 @@ const MIGRATION_DONE_KEY: &[u8] = b"HydrationConvictionVoting2sBlockMigrationDon
 //
 // The migration uses a raw storage marker to prevent accidental double execution. Make sure it is
 // removed from the Runtime Executive after it has been run.
-pub struct MigrateConvictionVotingTo2sBlocks<T: pallet::Config>(PhantomData<T>);
+pub struct MigrateConvictionVotingTo2sBlocks<T: pallet::Config<I>, I: 'static = ()>(PhantomData<(T, I)>);
 
-impl<T: pallet::Config> MigrateConvictionVotingTo2sBlocks<T> {
+impl<T: pallet::Config<I>, I: 'static> MigrateConvictionVotingTo2sBlocks<T, I> {
 	fn is_done() -> bool {
 		sp_io::storage::get(MIGRATION_DONE_KEY).is_some()
 	}
@@ -27,7 +28,7 @@ impl<T: pallet::Config> MigrateConvictionVotingTo2sBlocks<T> {
 		sp_io::storage::set(MIGRATION_DONE_KEY, &true.encode());
 	}
 
-	fn scale_future_block(block: BlockNumberFor<T>, current_block: BlockNumberFor<T>) -> BlockNumberFor<T> {
+	fn scale_future_block(block: BlockNumberFor<T, I>, current_block: BlockNumberFor<T, I>) -> BlockNumberFor<T, I> {
 		if block <= current_block {
 			block
 		} else {
@@ -36,27 +37,38 @@ impl<T: pallet::Config> MigrateConvictionVotingTo2sBlocks<T> {
 	}
 }
 
-impl<T: pallet::Config> OnRuntimeUpgrade for MigrateConvictionVotingTo2sBlocks<T> {
+impl<T: pallet::Config<I>, I: 'static> OnRuntimeUpgrade for MigrateConvictionVotingTo2sBlocks<T, I> {
 	fn on_runtime_upgrade() -> Weight {
 		if Self::is_done() {
 			log::warn!("MigrateConvictionVotingTo2sBlocks already executed");
 			return T::DbWeight::get().reads(1);
 		}
 
-		let current_block = T::BlockNumberProvider::current_block_number();
-		let mut reads = 1u64;
+		let current_block = <T as pallet::Config<I>>::BlockNumberProvider::current_block_number();
+		let voting_for_records = VotingFor::<T, I>::iter()
+			.take(MAX_VOTING_FOR_RECORDS.saturating_add(1) as usize)
+			.count() as u64;
+		let mut reads = 1u64.saturating_add(voting_for_records);
 		let mut writes = 0u64;
 		let mut migrated = 0u64;
 
-		for (who, class, mut voting) in VotingFor::<T>::iter() {
+		if voting_for_records > MAX_VOTING_FOR_RECORDS {
+			log::error!(
+				"MigrateConvictionVotingTo2sBlocks skipped because VotingFor has more than {:?} records",
+				MAX_VOTING_FOR_RECORDS
+			);
+			return T::DbWeight::get().reads(reads);
+		}
+
+		for (who, class, mut voting) in VotingFor::<T, I>::iter() {
 			reads.saturating_inc();
-			let prior: &mut PriorLock<BlockNumberFor<T>, _> = voting.as_mut();
+			let prior: &mut PriorLock<BlockNumberFor<T, I>, _> = voting.as_mut();
 			let old_until = prior.0;
 			let new_until = Self::scale_future_block(old_until, current_block);
 
 			if old_until != new_until {
 				prior.0 = new_until;
-				VotingFor::<T>::insert(who, class, voting);
+				VotingFor::<T, I>::insert(who, class, voting);
 				writes.saturating_inc();
 				migrated.saturating_inc();
 			}
@@ -76,8 +88,8 @@ mod test {
 	use crate::{AccountId, Runtime, System};
 	use pallet_conviction_voting::{Casting, Voting};
 
-	fn prior_until(who: AccountId) -> BlockNumberFor<Runtime> {
-		match VotingFor::<Runtime>::get(who, 0u16) {
+	fn prior_until(who: AccountId) -> BlockNumberFor<Runtime, ()> {
+		match VotingFor::<Runtime, ()>::get(who, 0u16) {
 			Voting::Casting(Casting { prior, .. }) => prior.0,
 			Voting::Delegating(delegating) => delegating.prior.0,
 		}
@@ -90,7 +102,7 @@ mod test {
 		ext.execute_with(|| {
 			let who = AccountId::new([1; 32]);
 			System::set_block_number(100);
-			VotingFor::<Runtime>::insert(
+			VotingFor::<Runtime, ()>::insert(
 				who.clone(),
 				0u16,
 				Voting::Casting(Casting {
@@ -102,7 +114,7 @@ mod test {
 
 			MigrateConvictionVotingTo2sBlocks::<Runtime>::on_runtime_upgrade();
 
-			let voting = VotingFor::<Runtime>::get(who.clone(), 0u16);
+			let voting = VotingFor::<Runtime, ()>::get(who.clone(), 0u16);
 			match voting {
 				Voting::Casting(Casting { prior, .. }) => {
 					assert_eq!(prior.0, 400);
