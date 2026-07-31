7af287b52700ef020a511efc78514bb366172dac
cl0w <v@lery.dev>
2026-07-31 14:33:58 +0200

    gigahdx migration storage version

--- pallets/gigahdx/src/migrations.rs
+++ pallets/gigahdx/src/migrations.rs
@@ -1,15 +1,34 @@
 use crate::pallet;
-use frame_support::{traits::OnRuntimeUpgrade, weights::Weight};
+use frame_support::{
+	traits::{OnRuntimeUpgrade, StorageVersion},
+	weights::Weight,
+};
 use sp_core::Get;
 
 pub struct SetTwoSecBlocksSince<T: pallet::Config>(sp_std::marker::PhantomData<T>);
 impl<T: pallet::Config> OnRuntimeUpgrade for SetTwoSecBlocksSince<T> {
 	#[cfg(feature = "try-runtime")]
 	fn pre_upgrade() -> Result<sp_std::vec::Vec<u8>, sp_runtime::TryRuntimeError> {
+		assert_eq!(
+			StorageVersion::get::<crate::Pallet<T>>(),
+			StorageVersion::new(1),
+			"GigaHdx storage version must be v1 before setting TwoSecBlocksSince"
+		);
+
 		Ok(sp_std::vec::Vec::new())
 	}
 
 	fn on_runtime_upgrade() -> Weight {
+		let on_chain_version = StorageVersion::get::<crate::Pallet<T>>();
+		if on_chain_version >= StorageVersion::new(2) {
+			return T::DbWeight::get().reads(1);
+		}
+
+		if on_chain_version != StorageVersion::new(1) {
+			log::warn!("GigaHdx 2s block migration skipped: expected storage version 1, got {on_chain_version:?}");
+			return T::DbWeight::get().reads(1);
+		}
+
 		let current_block_height = frame_system::Pallet::<T>::block_number();
 		let mut writes = 0u64;
 
@@ -23,7 +42,10 @@ impl<T: pallet::Config> OnRuntimeUpgrade for SetTwoSecBlocksSince<T> {
 			log::info!("GigaHdx TwoSecBlocksSince already set to: {two_sec_blocks_since:?}");
 		}
 
-		T::DbWeight::get().reads_writes(1, writes)
+		StorageVersion::new(2).put::<crate::Pallet<T>>();
+		writes += 1;
+
+		T::DbWeight::get().reads_writes(2, writes)
 	}
 
 	#[cfg(feature = "try-runtime")]
@@ -31,6 +53,11 @@ impl<T: pallet::Config> OnRuntimeUpgrade for SetTwoSecBlocksSince<T> {
 		assert!(
 			crate::TwoSecBlocksSince::<T>::get() != u32::MAX.into(),
 			"GigaHdx TwoSecBlocksSince must be initialized"
+		);
+		assert_eq!(
+			StorageVersion::get::<crate::Pallet<T>>(),
+			StorageVersion::new(2),
+			"GigaHdx storage version must be v2 after setting TwoSecBlocksSince"
 		);
 
 		Ok(())
@@ -50,10 +77,12 @@ mod test {
 	fn set_two_sec_blocks_since_executes_when_storage_not_set() {
 		ExtBuilder::default().build().execute_with(|| {
 			System::set_block_number(500);
+			StorageVersion::new(1).put::<GigaHdx>();
 
 			SetTwoSecBlocksSince::<Test>::on_runtime_upgrade();
 
 			assert_eq!(GigaHdx::two_sec_blocks_since(), 500);
+			assert_eq!(StorageVersion::get::<GigaHdx>(), StorageVersion::new(2));
 		});
 	}
 
@@ -61,11 +90,13 @@ mod test {
 	fn set_two_sec_blocks_since_does_not_overwrite_existing_value() {
 		ExtBuilder::default().build().execute_with(|| {
 			System::set_block_number(500);
+			StorageVersion::new(1).put::<GigaHdx>();
 			TwoSecBlocksSince::<Test>::put(123);
 
 			SetTwoSecBlocksSince::<Test>::on_runtime_upgrade();
 
 			assert_eq!(GigaHdx::two_sec_blocks_since(), 123);
+			assert_eq!(StorageVersion::get::<GigaHdx>(), StorageVersion::new(2));
 		});
 	}
 }
