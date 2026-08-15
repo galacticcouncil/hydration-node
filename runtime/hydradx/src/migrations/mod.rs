8d0309fc8260fa0ccf767e37546a69d490c83c88
vgantchev <v@lery.dev>
2026-08-15 11:54:55 +0200

    redistribute dca schedules to respect new per-block limit

--- runtime/hydradx/src/migrations/mod.rs
+++ runtime/hydradx/src/migrations/mod.rs
@@ -26,7 +26,7 @@ pub mod stableswap;
 // New migrations which need to be cleaned up after every Runtime upgrade
 pub type UnreleasedSingleBlockMigrations = (
 	parameters::SetTwoSecBlocksSince<Runtime>,
-	pallet_dca::migrations::MultiplySchedulesPeriodBy3<Runtime>,
+	pallet_dca::migrations::MigrateSchedulesTo2sBlocks<Runtime>,
 	circuit_breaker::MigrateCircuitBreakerTo2sBlocks<Runtime>,
 	dynamic_fees::MigrateDynamicFeeDecayTo2sBlocks<Runtime>,
 	stableswap::MigrateStableswapMaxPegUpdateTo2sBlocks<Runtime>,
